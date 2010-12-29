f8d42fb6a0e330bc8ae2cd96f41eb0fa81d63c10
ruki <waruqi@gmail.com>
2010-12-29 10:33:42 +0800

    ...

--- src/demo/linux_x86/dns.c
+++ src/demo/linux_x86/dns.c
@@ -6,7 +6,6 @@ int main(int argc, char** argv)
 	tplat_size_t regular_block_n[TPLAT_POOL_REGULAR_CHUNCK_MAX_COUNT] = {10, 10, 10, 10, 10, 10, 10};
 	tplat_pool_create(TB_CONFIG_MEMORY_POOL_INDEX, malloc(1024 * 1024), 1024 * 1024, regular_block_n);
 
-#error block
 	tb_dns_server_add("211.95.1.123");
 	tb_dns_server_add("180.168.255.18");
 	tb_dns_server_add("180.168.255.118");
