43b376e47cc1369b32963a6e8583adef0f428922
ruki <waruqi@gmail.com>
2011-11-06 23:58:02 +0800

    rename trace tag

--- src/prefix/trace.h
+++ src/prefix/trace.h
@@ -37,14 +37,11 @@ extern "C" {
  * macros
  */
 
-// tag
-#define TB_TAG 											"tb"
-
 #if 1
 // print
 #ifndef TB_CONFIG_COMPILER_NOT_SUPPORT_VARARG_MACRO
 #	define TB_PRINT_TAG(tag, fmt, arg...)				do { tb_printf("["tag"]: " fmt "\n" , ## arg); } while (0)
-# 	define TB_PRINT(fmt, arg...)						TB_PRINT_TAG(TB_TAG, fmt, ## arg)
+# 	define TB_PRINT(fmt, arg...)						TB_PRINT_TAG(TB_TRACE_TAG, fmt, ## arg)
 #else
 #	define TB_PRINT_TAG
 #	define TB_PRINT
@@ -52,19 +49,19 @@ extern "C" {
 
 // trace
 #if defined(TB_DEBUG) && !defined(TB_CONFIG_COMPILER_NOT_SUPPORT_VARARG_MACRO)
-#	define TB_TRACE_TAG(tag, fmt, arg...)				TB_PRINT_TAG(tag, fmt, ## arg)
+//#	define TB_TRACE_TAG(tag, fmt, arg...)				TB_PRINT_TAG(tag, fmt, ## arg)
 # 	define TB_TRACE(fmt, arg...)						TB_PRINT(fmt, ## arg)
 # 	define TB_TRACE_LINE_TAG(tag, fmt, arg...) 			TB_PRINT_TAG(tag, fmt "at func: %s, line: %d, file: %s", ##arg, __tb_func__, __tb_line__, __tb_file__)
-# 	define TB_TRACE_LINE(fmt, arg...) 					TB_TRACE_LINE_TAG(TB_TAG, fmt, ## arg)
+# 	define TB_TRACE_LINE(fmt, arg...) 					TB_TRACE_LINE_TAG(TB_TRACE_TAG, fmt, ## arg)
 # 	define TB_DBG(fmt, arg...)							TB_TRACE(fmt, ## arg) 				//!< discarded
 #elif !defined(TB_CONFIG_COMPILER_NOT_SUPPORT_VARARG_MACRO)
-#	define TB_TRACE_TAG(...)
+//#	define TB_TRACE_TAG(...)
 # 	define TB_TRACE(...)
 # 	define TB_TRACE_LINE_TAG(...)
 # 	define TB_TRACE_LINE(...)
 # 	define TB_DBG(...)							
 #else
-#	define TB_TRACE_TAG
+//#	define TB_TRACE_TAG
 # 	define TB_TRACE
 # 	define TB_TRACE_LINE_TAG
 # 	define TB_TRACE_LINE
@@ -72,7 +69,7 @@ extern "C" {
 #endif
 
 #define TB_TRACE_NOIMPL_TAG(tag) 						TB_TRACE_LINE_TAG(tag, "[no_impl]: ")
-#define TB_TRACE_NOIMPL() 								TB_TRACE_NOIMPL_TAG(TB_TAG)
+#define TB_TRACE_NOIMPL() 								TB_TRACE_NOIMPL_TAG(TB_TRACE_TAG)
 #define TB_NOT_IMPLEMENT() 								TB_TRACE_NOIMPL() 				//!< discarded
 #endif
 
@@ -80,7 +77,7 @@ extern "C" {
 // print
 #ifndef TB_CONFIG_COMPILER_NOT_SUPPORT_VARARG_MACRO
 #	define tb_print_tag(tag, fmt, arg...)				do { tb_printf("["tag"]: " fmt "\n" , ## arg); } while (0)
-# 	define tb_print(fmt, arg...)						tb_print_tag(TB_TAG, fmt, ## arg)
+# 	define tb_print(fmt, arg...)						tb_print_tag(TB_TRACE_TAG, fmt, ## arg)
 #else
 #	define tb_print_tag
 #	define tb_print
@@ -99,8 +96,8 @@ extern "C" {
 #endif
 
 #ifndef TB_CONFIG_COMPILER_NOT_SUPPORT_VARARG_MACRO
-# 	define tb_trace(fmt, arg...)						tb_trace_tag(TB_TAG, fmt, ## arg)
-# 	define tb_trace_line(fmt, arg...) 					tb_trace_line_tag(TB_TAG, fmt, ## arg)
+# 	define tb_trace(fmt, arg...)						tb_trace_tag(TB_TRACE_TAG, fmt, ## arg)
+# 	define tb_trace_line(fmt, arg...) 					tb_trace_line_tag(TB_TRACE_TAG, fmt, ## arg)
 #else
 # 	define tb_trace
 # 	define tb_trace_line
