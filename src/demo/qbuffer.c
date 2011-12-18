37303291409c5fa68603879b1e3295c35b3e5522
ruki <waruqi@gmail.com>
2011-12-18 18:16:48 +0800

    impl qbuffer demo

--- src/demo/qbuffer.c
+++ src/demo/qbuffer.c
@@ -10,9 +10,15 @@ int main(int argc, char** argv)
 {
 	if (!tb_init(malloc(1024 * 1024), 1024 * 1024)) return 0;
 
-	tb_qbuffer_t b;
-	tb_qbuffer_init(&b);
+	tb_char_t 		d[1024];
+	tb_qbuffer_t 	b;
+	tb_qbuffer_init(&b, 1024);
 
+	tb_qbuffer_writ(&b, "hello", 5);
+	tb_qbuffer_writ(&b, " ", 1);
+	tb_qbuffer_writ(&b, "world", 6);
+	tb_qbuffer_read(&b, d, 1024);
+	tb_print("%s", d);
 
 	tb_qbuffer_exit(&b);
 
