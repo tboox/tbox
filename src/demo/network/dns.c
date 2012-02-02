07f11fa4f463c1d3e50a1dd29ba6481555924f9d
ruki <waruqi@gmail.com>
2012-02-02 09:56:03 +0000

    add ipv4 and modify dns

--- src/demo/network/dns.c
+++ src/demo/network/dns.c
@@ -11,6 +11,7 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
 	// init
 	if (!tb_init(malloc(1024 * 1024), 1024 * 1024)) return 0;
 
+	// list
 #if 0
 	tb_dns_list_adds("211.95.1.123");
 	tb_dns_list_adds("180.168.255.18");
@@ -21,6 +22,11 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
 #ifdef TB_DEBUG
 	tb_dns_list_dump();
 #endif
+
+	// done
+	tb_char_t 			data[16];
+	tb_char_t const* 	addr = tb_dns_name_done(argv[1], data, 16);
+	tb_print("[demo]: %s => %s", argv[1], addr);
 
 	// exit
 	tb_exit();
