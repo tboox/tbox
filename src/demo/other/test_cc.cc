9a5adf396e451b2f111c9f83a986dad6de470c07
waruqi <waruqi@gmail.com>
2012-11-16 16:01:14 +0800

    add some demo files

---
+++ src/demo/other/test_cc.cc
@@ -0,0 +1,27 @@
+#include "tbox.h"
+
+/* ///////////////////////////////////////////////////////////////////////
+ * test
+ */
+static tb_int_t test(tb_int_t argc, tb_char_t** argv)
+{
+	__tb_volatile__ tb_int_t* a = new tb_int_t[10];
+	__tb_volatile__ tb_int_t* b = new tb_int_t;
+	__tb_volatile__ tb_int_t* c = new tb_int_t;
+	
+	delete[] a;
+
+	//tb_memory_dump();
+	return 0;
+}
+
+/* ///////////////////////////////////////////////////////////////////////
+ * main
+ */
+extern "C"
+{
+	tb_int_t test_cc_main(tb_int_t argc, tb_char_t** argv)
+	{
+		return test(argc, argv);
+	}
+}
