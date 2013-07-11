d94313a4e9bd314f7a092e967ec3ebb529c89233
ruki <waruqi@gmail.com>
2013-07-11 18:53:16 +0800

    modify exception interface name

--- src/demo/other/exception.c
+++ src/demo/other/exception.c
@@ -13,29 +13,29 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
 	if (!tb_init(malloc(1024 * 1024), 1024 * 1024)) return 0;
 
 	tb_size_t i = 0;
-	__tb_try__ 
+	__tb_try 
 	{
 		tb_print("try0: b: %lu", i++);
 
-		__tb_try__ 
+		__tb_try 
 		{
 			tb_print("try1: b: %lu", i++);
 //			tb_abort();
 			tb_print("try1: e: %lu", i++);
 		}
-		__tb_except__
+		__tb_except
 		{
 			tb_print("except1: %lu", i++);
 		}
-		__tb_end__
+		__tb_end
 
 		tb_print("end1: %lu", i);
 	}
-	__tb_except__
+	__tb_except
 	{
 		tb_print("except0: %lu", i++);
 	}
-	__tb_end__
+	__tb_end
 
 	tb_print("end0: %lu", i);
 
