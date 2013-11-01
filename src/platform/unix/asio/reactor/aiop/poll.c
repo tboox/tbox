059af0bbc3a9c14519644de32a21a98e485df899
ruki <waruqi@gmail.com>
2013-11-01 17:49:59 +0800

    impl aiop multi-thread safe

--- src/platform/unix/asio/reactor/aiop/poll.c
+++ src/platform/unix/asio/reactor/aiop/poll.c
@@ -38,6 +38,12 @@ typedef struct __tb_aiop_reactor_poll_t
 	// the poll fds
 	tb_vector_t* 			pfds;
 
+	// the copy fds
+	tb_vector_t* 			cfds;
+
+	// the mutx
+	tb_handle_t 			mutx;
+
 }tb_aiop_reactor_poll_t;
 
 /* ///////////////////////////////////////////////////////////////////////
@@ -59,7 +65,9 @@ static tb_bool_t tb_aiop_reactor_poll_addo(tb_aiop_reactor_t* reactor, tb_handle
 	if (aioe & TB_AIOE_CODE_SEND || aioe & TB_AIOE_CODE_CONN) pfd.events |= POLLOUT;
 
 	// add pfd
+	if (rtor->mutx) tb_mutex_enter(rtor->mutx);
 	tb_vector_insert_tail(rtor->pfds, &pfd);
+	if (rtor->mutx) tb_mutex_leave(rtor->mutx);
 
 	// ok
 	return tb_true;
@@ -72,6 +80,9 @@ static tb_bool_t tb_aiop_reactor_poll_seto(tb_aiop_reactor_t* reactor, tb_handle
 	// fd
 	tb_long_t fd = ((tb_long_t)handle) - 1;
 	tb_assert_and_check_return_val(fd >= 0, tb_false);
+
+	// enter
+	if (rtor->mutx) tb_mutex_enter(rtor->mutx);
 
 	// set pfd
 	tb_size_t itor = tb_iterator_head(rtor->pfds);
@@ -87,10 +98,12 @@ static tb_bool_t tb_aiop_reactor_poll_seto(tb_aiop_reactor_t* reactor, tb_handle
 			break;
 		}
 	}
-	tb_assert_and_check_return_val(itor != tail, tb_false);
+
+	// leave
+	if (rtor->mutx) tb_mutex_leave(rtor->mutx);
 
 	// ok
-	return tb_true;
+	return itor != tail? tb_true : tb_false;
 }
 static tb_bool_t tb_aiop_reactor_poll_delo(tb_aiop_reactor_t* reactor, tb_handle_t handle)
 {
@@ -101,6 +114,9 @@ static tb_bool_t tb_aiop_reactor_poll_delo(tb_aiop_reactor_t* reactor, tb_handle
 	tb_long_t fd = ((tb_long_t)handle) - 1;
 	tb_assert_and_check_return_val(fd >= 0, tb_false);
 
+	// enter
+	if (rtor->mutx) tb_mutex_enter(rtor->mutx);
+
 	// find pfd
 	tb_size_t itor = tb_iterator_head(rtor->pfds);
 	tb_size_t tail = tb_iterator_tail(rtor->pfds);
@@ -109,37 +125,48 @@ static tb_bool_t tb_aiop_reactor_poll_delo(tb_aiop_reactor_t* reactor, tb_handle
 		struct pollfd* pfd = (struct pollfd*)tb_iterator_item(rtor->pfds, itor);
 		if (pfd && pfd->fd == fd) break;
 	}
-	tb_assert_and_check_return_val(itor != tail, tb_false);
-	
+
 	// del pfd
-	tb_vector_remove(rtor->pfds, itor);
+	if (itor != tail) tb_vector_remove(rtor->pfds, itor);
+
+	// leave
+	if (rtor->mutx) tb_mutex_leave(rtor->mutx);
 
 	// ok
-	return tb_true;
+	return itor != tail? tb_true : tb_false;
 }
 static tb_long_t tb_aiop_reactor_poll_wait(tb_aiop_reactor_t* reactor, tb_aioo_t* objs, tb_size_t objm, tb_long_t timeout)
 {	
 	tb_aiop_reactor_poll_t* rtor = (tb_aiop_reactor_poll_t*)reactor;
 	tb_assert_and_check_return_val(rtor && rtor->pfds && reactor->aiop && reactor->aiop->hash, -1);
 
-	// pfds
-	struct pollfd* 	pfds = (struct pollfd*)tb_vector_data(rtor->pfds);
-	tb_size_t 		pfdm = tb_vector_size(rtor->pfds);
-	tb_assert_and_check_return_val(pfds && pfdm, -1);
+	// enter
+	if (rtor->mutx) tb_mutex_enter(rtor->mutx);
+
+	// copy pfds
+	tb_vector_copy(rtor->cfds, rtor->pfds);
+
+	// leave
+	if (rtor->mutx) tb_mutex_leave(rtor->mutx);
+
+	// cfds
+	struct pollfd* 	cfds = (struct pollfd*)tb_vector_data(rtor->cfds);
+	tb_size_t 		cfdm = tb_vector_size(rtor->cfds);
+	tb_assert_and_check_return_val(cfds && cfdm, -1);
 
 	// wait
-	tb_long_t pfdn = poll(pfds, pfdm, timeout);
-	tb_assert_and_check_return_val(pfdn >= 0, -1);
+	tb_long_t cfdn = poll(cfds, cfdm, timeout);
+	tb_assert_and_check_return_val(cfdn >= 0, -1);
 
 	// timeout?
-	tb_check_return_val(pfdn, 0);
+	tb_check_return_val(cfdn, 0);
 
 	// sync
 	tb_size_t i = 0;
 	tb_size_t n = 0;
-	for (i = 0; i < pfdm && n < objm; i++)
+	for (i = 0; i < cfdm && n < objm; i++)
 	{
-		struct pollfd* 	p = pfds + i;
+		struct pollfd* 	p = cfds + i;
 		tb_aioo_t* 	o = tb_hash_get(reactor->aiop->hash, (p->fd + 1));
 		tb_assert_and_check_return_val(o, -1);
 		
@@ -171,7 +198,18 @@ static tb_void_t tb_aiop_reactor_poll_exit(tb_aiop_reactor_t* reactor)
 	if (rtor)
 	{
 		// exit pfds
+		if (rtor->mutx) tb_mutex_enter(rtor->mutx);
 		if (rtor->pfds) tb_vector_exit(rtor->pfds);
+		rtor->pfds = tb_null;
+		if (rtor->mutx) tb_mutex_leave(rtor->mutx);
+
+		// exit cfds
+		if (rtor->cfds) tb_vector_exit(rtor->cfds);
+		rtor->cfds = tb_null;
+
+		// exit mutx
+		if (rtor->mutx) tb_mutex_exit(rtor->mutx);
+		rtor->mutx = tb_null;
 
 		// free it
 		tb_free(rtor);
@@ -183,7 +221,9 @@ static tb_void_t tb_aiop_reactor_poll_cler(tb_aiop_reactor_t* reactor)
 	if (rtor)
 	{
 		// clear pfds
+		if (rtor->mutx) tb_mutex_enter(rtor->mutx);
 		if (rtor->pfds) tb_vector_clear(rtor->pfds);
+		if (rtor->mutx) tb_mutex_leave(rtor->mutx);
 	}
 }
 static tb_aiop_reactor_t* tb_aiop_reactor_poll_init(tb_aiop_t* aiop)
@@ -204,9 +244,17 @@ static tb_aiop_reactor_t* tb_aiop_reactor_poll_init(tb_aiop_t* aiop)
 	rtor->base.delo = tb_aiop_reactor_poll_delo;
 	rtor->base.wait = tb_aiop_reactor_poll_wait;
 
+	// init mutx
+	rtor->mutx = tb_mutex_init(tb_null);
+	tb_assert_and_check_goto(rtor->mutx, fail);
+
 	// init pfds
 	rtor->pfds = tb_vector_init(tb_align8((aiop->maxn >> 3) + 1), tb_item_func_ifm(sizeof(struct pollfd), tb_null, tb_null));
 	tb_assert_and_check_goto(rtor->pfds, fail);
+
+	// init cfds
+	rtor->cfds = tb_vector_init(tb_align8((aiop->maxn >> 3) + 1), tb_item_func_ifm(sizeof(struct pollfd), tb_null, tb_null));
+	tb_assert_and_check_goto(rtor->cfds, fail);
 
 	// ok
 	return (tb_aiop_reactor_t*)rtor;
