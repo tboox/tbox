9b0aef50c638e28dbb745ad0d49930954a5f8d79
ruki <waruqi@gmail.com>
2013-12-05 16:45:58 +0800

    reimpl timer using miniheap

--- src/demo/platform/timer.c
+++ src/demo/platform/timer.c
@@ -8,13 +8,18 @@
 /* ///////////////////////////////////////////////////////////////////////
  * func
  */
-static tb_bool_t tb_timer_task_func(tb_pointer_t data)
+static tb_void_t tb_timer_task_func(tb_pointer_t data)
 {
-	// trace
-	tb_print("task[%s]: %lld", (tb_char_t const*)data, tb_mclock());
+	// get the time
+	tb_timeval_t tv = {0};
+    if (tb_gettimeofday(&tv, tb_null))
+	{
+		// the time value
+		tb_hong_t val = ((tb_hong_t)tv.tv_sec * 1000 + tv.tv_usec / 1000);
 
-	// continue?
-	return !tb_strcmp(data, "one")? tb_false : tb_true;
+		// trace
+		tb_print("task[%s]: %lld ms", (tb_char_t const*)data, val);
+	}
 }
 
 /* ///////////////////////////////////////////////////////////////////////
@@ -23,21 +28,22 @@ static tb_bool_t tb_timer_task_func(tb_pointer_t data)
 tb_int_t main(tb_int_t argc, tb_char_t** argv)
 {
 	// init
-	if (!tb_init(malloc(1024 * 1024), 1024 * 1024)) return 0;
+	if (!tb_init(malloc(10 * 1024 * 1024), 10 * 1024 * 1024)) return 0;
 
 	// init timer
-	tb_handle_t timer = tb_timer_init(3, tb_true);
-//	tb_handle_t timer = tb_timer_init(3, tb_false);
+//	tb_handle_t timer = tb_timer_init(3, tb_true);
+	tb_handle_t timer = tb_timer_init(3, tb_false);
+
 	if (timer)
 	{
 		// add task: every
-		tb_timer_task_run(timer, 1000, tb_timer_task_func, "every");
+		tb_timer_task_run(timer, 1000, tb_true, tb_timer_task_func, "every");
 	
 		// add task: one
-		tb_timer_task_run(timer, 10000, tb_timer_task_func, "one");
+		tb_timer_task_run(timer, 10000, tb_false, tb_timer_task_func, "one");
 		
 		// add task: after
-		tb_timer_task_run_after(timer, 10000, 5000, tb_timer_task_func, "after");
+		tb_timer_task_run_after(timer, 10000, 5000, tb_true, tb_timer_task_func, "after");
 
 		// wait
 		tb_timer_loop(timer);
