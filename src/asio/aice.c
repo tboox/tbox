b22f7fe4af2a325a055f32143823c98889be140d
ruki <waruqi@gmail.com>
2013-12-10 18:31:36 +0800

    add aice state cstr interface

---
+++ src/asio/aice.c
@@ -0,0 +1,40 @@
+/*!The Treasure Box Library
+ * 
+ * TBox is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * TBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TBox; 
+ * If not, see <a href="http://www.gnu.org/licenses/"> http://www.gnu.org/licenses/</a>
+ * 
+ * Copyright (C) 2009 - 2012, ruki All rights reserved.
+ *
+ * @author		ruki
+ * @file		aice.c
+ * @ingroup 	asio
+ *
+ */
+
+/* ///////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "aice.h"
+
+/* ///////////////////////////////////////////////////////////////////////
+ * implementation
+ */
+tb_char_t const* tb_aice_state_cstr(tb_aice_t const* aice)
+{
+	// check
+	tb_assert_and_check_return_val(aice, tb_null);
+
+	return tb_null;
+}
+
