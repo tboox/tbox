f45236d2ddd521666436bd8caadc30fa84e931fd
ruki <waruqi@gmail.com>
2014-01-11 22:17:15 +0800

    impl sock astream

--- src/demo/other/test.c
+++ src/demo/other/test.c
@@ -8,7 +8,15 @@
  */
 tb_int_t tb_demo_other_test_main(tb_int_t argc, tb_char_t** argv)
 {
-	// abort
-	tb_abort();
+
+	tb_hong_t t = tb_uclock();
+	__tb_volatile__ tb_size_t n = 0;
+	__tb_volatile__ tb_size_t i = 0;
+	for (i = 0; i < 18; i++)
+	{
+		if (i) n++;
+	}
+	t = tb_uclock() - t;
+	tb_print("%lld", t);
 	return 0;
 }
