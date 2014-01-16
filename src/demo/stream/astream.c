a0df3ae22eb9df08df25e7449dda9a8333623a0f
ruki <waruqi@gmail.com>
2014-01-16 22:16:23 +0800

    reimpl astream demo

--- src/demo/stream/astream.c
+++ src/demo/stream/astream.c
@@ -28,16 +28,25 @@ typedef struct __tb_demo_context_t
 	// is verbose
 	tb_bool_t 				verbose;
 
+	// the istream
+	tb_astream_t* 			istream;
+
+	// the ostream
+	tb_astream_t* 			ostream;
+
+	// the tstream
+	tb_handle_t 			tstream;
+
 }tb_demo_context_t;
 
 /* ///////////////////////////////////////////////////////////////////////
  * func
  */
-static tb_bool_t tb_demo_istream_save_func(tb_astream_t* ast, tb_astream_t* ost, tb_size_t state, tb_size_t size, tb_pointer_t priv)
+static tb_bool_t tb_demo_tstream_save_func(tb_size_t state, tb_size_t size, tb_size_t rate, tb_pointer_t priv)
 {
 	// check
 	tb_demo_context_t* context = (tb_demo_context_t*)priv;
-	tb_assert_and_check_return_val(ast && context && context->option, tb_false);
+	tb_assert_and_check_return_val(context && context->option, tb_false);
 
 	// ok
 	if (state == TB_ASTREAM_STATE_OK)
@@ -53,7 +62,7 @@ static tb_bool_t tb_demo_istream_save_func(tb_astream_t* ast, tb_astream_t* ost,
 		{
 			if (tb_mclock() - context->basc > 1000) 
 			{
-				tb_printf("save: %llu bytes, speed: %llu bytes / s\n", context->size, (context->size * 1000) / (tb_mclock() - context->base));
+				tb_printf("save: %llu bytes, rate: %llu bytes/s\n", context->size, rate);
 				context->basc = tb_mclock();
 			}
 		}
@@ -63,14 +72,11 @@ static tb_bool_t tb_demo_istream_save_func(tb_astream_t* ast, tb_astream_t* ost,
 	else
 	{
 		// print verbose info
-		if (context->verbose) tb_printf("save: %llu bytes, speed: %llu bytes / s, state: %s\n", context->size, (context->size * 1000) / (tb_mclock() - context->base), tb_astream_state_cstr(state));
+		if (context->verbose) tb_printf("save: %llu bytes, rate: %llu bytes / s, state: %s\n", context->size, rate, tb_astream_state_cstr(state));
 
 		// kill aicp
-		tb_aicp_kill(tb_astream_aicp(ast));
+		tb_aicp_kill(tb_astream_aicp(context->istream));
 	}
-
-	// closed or failed or finished?
-	if (state != TB_ASTREAM_STATE_OK) tb_astream_exit(ost);
 
 	// ok?
 	return (state == TB_ASTREAM_STATE_OK)? tb_true : tb_false;
@@ -83,7 +89,6 @@ static tb_bool_t tb_demo_istream_open_func(tb_astream_t* ast, tb_size_t state, t
 
 	// done
 	tb_bool_t 		ok = tb_false;
-	tb_astream_t* 	stream = tb_null;
 	do
 	{
 		// check
@@ -102,14 +107,14 @@ static tb_bool_t tb_demo_istream_open_func(tb_astream_t* ast, tb_size_t state, t
 		// trace
 		if (context->verbose) tb_printf("open: ok\n");
 
-		// init stream
+		// init ostream
 		if (tb_option_find(context->option, "more0"))
 		{
 			// the path
 			tb_char_t const* path = tb_option_item_cstr(context->option, "more0");
 
 			// init
-			stream = tb_astream_init_from_url(tb_astream_aicp(ast), path);
+			context->ostream = tb_astream_init_from_url(tb_astream_aicp(ast), path);
 
 			// print verbose info
 			if (context->verbose) tb_printf("save: %s: ..\n", path);
@@ -129,52 +134,28 @@ static tb_bool_t tb_demo_istream_open_func(tb_astream_t* ast, tb_size_t state, t
 				tb_strcat(path, name);
 
 				// init file
-				stream = tb_astream_init_from_url(tb_astream_aicp(ast), path);
+				context->ostream = tb_astream_init_from_url(tb_astream_aicp(ast), path);
 
 				// print verbose info
 				if (context->verbose) tb_printf("save: %s: ..\n", path);
 			}
 		}
-		tb_assert_and_check_break(stream);
+		tb_assert_and_check_break(context->ostream);
 
-#if 0
-		// ctrl stream
-		if (tb_astream_type(stream) == TB_ASTREAM_TYPE_FILE) 
-			tb_astream_ctrl(stream, TB_ASTREAM_CTRL_FILE_SET_MODE, TB_FILE_MODE_WO | TB_FILE_MODE_CREAT | TB_FILE_MODE_TRUNC);
+		// init tstream
+		context->tstream = tb_tstream_init_aa(ast, context->ostream, tb_demo_tstream_save_func, context);
+		tb_assert_and_check_break(context->tstream);
 
-		// try to open stream only for file
-		if (!tb_astream_try_open(stream)) 
-		{
-			if (context->verbose) 
-			{	
-				tb_char_t const* url = tb_null;
-				tb_astream_ctrl(stream, TB_ASTREAM_CTRL_GET_URL, &url);
-				tb_printf("open: %s: failed\n", url);
-			}
-			break;
-		}
-	
-		// save stream
-		context->base = tb_mclock();
-		context->basc = context->base;
-		context->size = 0;
-//		if (!tb_astream_save(ast, stream, tb_demo_istream_save_func, context)) break;
-#endif
+		// start tstream
+		if (!tb_tstream_start(context->tstream, -1)) break;
 
 		// ok
 		ok = tb_true;
 
 	} while (0);
 
-	// failed?
-	if (!ok) 
-	{
-		// exit stream
-		if (stream) tb_astream_exit(stream);
-
-		// kill aicp
-		tb_aicp_kill(tb_astream_aicp(ast));
-	}
+	// failed? // kill aicp
+	if (!ok) tb_aicp_kill(tb_astream_aicp(ast));
 
 	// ok?
 	return ok;
@@ -205,7 +186,6 @@ tb_int_t tb_demo_stream_astream_main(tb_int_t argc, tb_char_t** argv)
 {
 	// done
 	tb_aicp_t* 			aicp = tb_null;
-	tb_astream_t* 		stream = tb_null;
 	tb_demo_context_t 	context = {0};
 	do
 	{
@@ -227,22 +207,22 @@ tb_int_t tb_demo_stream_astream_main(tb_int_t argc, tb_char_t** argv)
 				aicp = tb_aicp_init(2);
 				tb_assert_and_check_break(aicp);
 
-				// init stream
-				stream = tb_astream_init_from_url(aicp, tb_option_item_cstr(context.option, "url"));
-				tb_assert_and_check_break(stream);
+				// init istream
+				context.istream = tb_astream_init_from_url(aicp, tb_option_item_cstr(context.option, "url"));
+				tb_assert_and_check_break(context.istream);
 
 				// set timeout
 				if (tb_option_find(context.option, "timeout"))
 				{
 					tb_size_t timeout = tb_option_item_uint32(context.option, "timeout");
-					tb_astream_ctrl(stream, TB_ASTREAM_CTRL_SET_TIMEOUT, &timeout);
+					tb_astream_ctrl(context.istream, TB_ASTREAM_CTRL_SET_TIMEOUT, &timeout);
 				}
 
 				// print verbose info
 				if (context.verbose) tb_printf("open: %s: ..\n", tb_option_item_cstr(context.option, "url"));
 
-				// open stream
-				if (!tb_astream_open(stream, tb_demo_istream_open_func, &context)) 
+				// open istream
+				if (!tb_astream_open(context.istream, tb_demo_istream_open_func, &context)) 
 				{
 					// print verbose info
 					if (context.verbose) tb_printf("open: failed\n");
@@ -258,17 +238,25 @@ tb_int_t tb_demo_stream_astream_main(tb_int_t argc, tb_char_t** argv)
 
 	} while (0);
 
-	// exit istream
-	if (stream) tb_astream_exit(stream);
-	stream = tb_null;
+	// exit tstream
+	if (context.tstream) tb_tstream_exit(context.tstream);
+	context.tstream = tb_null;
 
-	// exit aicp
-	if (aicp) tb_aicp_exit(aicp);
-	aicp = tb_null;
+	// exit istream
+	if (context.istream) tb_astream_exit(context.istream);
+	context.istream = tb_null;
+
+	// exit ostream
+	if (context.ostream) tb_astream_exit(context.ostream);
+	context.ostream = tb_null;
 
 	// exit option
 	if (context.option) tb_option_exit(context.option);
 	context.option = tb_null;
+
+	// exit aicp
+	if (aicp) tb_aicp_exit(aicp);
+	aicp = tb_null;
 
 	return 0;
 }
--- src/demo/stream/tstream.c
+++ src/demo/stream/tstream.c
@@ -4,14 +4,28 @@
 #include "../demo.h"
 
 /* ///////////////////////////////////////////////////////////////////////
- * func
+ * implementation
  */ 
 static tb_bool_t tb_demo_tstream_save_func(tb_size_t state, tb_size_t size, tb_size_t rate, tb_pointer_t priv)
 {
+	// trace
+	tb_print("save: %lu bytes, rate: %lu bytes/s, state: %s", size, rate, tb_astream_state_cstr(state));
 
-	return tb_false;
+	// ok
+	return tb_true;
 }
+static tb_pointer_t tb_demo_tstream_loop(tb_pointer_t data)
+{
+	// the aicp
+	tb_aicp_t* aicp = (tb_aicp_t*)data;
 
+	// done loop
+	if (aicp) tb_aicp_loop(aicp);
+
+	// exit it
+	tb_thread_return(tb_null);
+	return tb_null;
+}
 /* ///////////////////////////////////////////////////////////////////////
  * main
  */ 
@@ -30,11 +44,26 @@ tb_int_t tb_demo_stream_tstream_main(tb_int_t argc, tb_char_t** argv)
 		tstream = tb_tstream_init_uu(aicp, argv[1], argv[2], tb_demo_tstream_save_func, tb_null);
 		tb_assert_and_check_break(tstream);
 
+		// init loop
+		if (!tb_thread_init(tb_null, tb_demo_tstream_loop, aicp, 0)) break;
+
 		// start tstream
 		if (!tb_tstream_start(tstream, -1)) break;
 
-		// loop aicp
-		tb_aicp_loop(aicp);
+		// wait
+		getchar();
+
+		// pause tstream
+		tb_tstream_pause(tstream);
+
+		// wait
+		getchar();
+
+		// start tstream
+		if (!tb_tstream_start(tstream, -1)) break;
+
+		// wait
+		getchar();
 
 	} while (0);
 
