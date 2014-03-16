7713c191919983c569b8a8c34a9ff4a641c29116
ruki <waruqi@gmail.com>
2014-03-16 17:40:38 +0800

    rebuild bin object reader and writer

--- src/object/writer/prefix.h
+++ src/object/writer/prefix.h
@@ -63,5 +63,62 @@ static __tb_inline__ tb_bool_t tb_object_writer_newline(tb_gstream_t* stream, tb
 	// ok
 	return tb_true;
 }
+static __tb_inline__ tb_bool_t tb_object_writer_bin_type_size(tb_gstream_t* stream, tb_size_t type, tb_uint64_t size)
+{
+	// check
+	tb_assert_and_check_return_val(stream && type <= 0xff, tb_false);
+
+	// byte for size < 64bits
+	tb_size_t sizeb = tb_object_writer_need_bytes(size);
+	tb_assert_and_check_return_val(sizeb <= 8, tb_false);
+
+	// flag for size
+	tb_size_t sizef = 0;
+	switch (sizeb)
+	{
+	case 1: sizef = 0xc; break;
+	case 2: sizef = 0xd; break;
+	case 4: sizef = 0xe; break;
+	case 8: sizef = 0xf; break;
+	default: break;
+	}
+	tb_assert_and_check_return_val(sizef, tb_false);
+
+	// writ flag 
+	tb_uint8_t flag = ((type < 0xf? (tb_uint8_t)type : 0xf) << 4) | (size < 0xc? (tb_uint8_t)size : sizef);
+	if (!tb_gstream_bwrit_u8(stream, flag)) return tb_false;
+
+	// trace
+//	tb_trace("writ: type: %lu, size: %llu", type, size);
+
+	// writ type
+	if (type >= 0xf) if (!tb_gstream_bwrit_u8(stream, (tb_uint8_t)type)) return tb_false;
+
+	// writ size
+	if (size >= 0xc)
+	{
+		switch (sizeb)
+		{
+		case 1:
+			if (!tb_gstream_bwrit_u8(stream, (tb_uint8_t)size)) return tb_false;
+			break;
+		case 2:
+			if (!tb_gstream_bwrit_u16_be(stream, (tb_uint16_t)size)) return tb_false;
+			break;
+		case 4:
+			if (!tb_gstream_bwrit_u32_be(stream, (tb_uint32_t)size)) return tb_false;
+			break;
+		case 8:
+			if (!tb_gstream_bwrit_u64_be(stream, (tb_uint64_t)size)) return tb_false;
+			break;
+		default:
+			tb_assert_and_check_return_val(0, tb_false);
+			break;
+		}
+	}
+
+	// ok
+	return tb_true;
+}
 
 #endif
