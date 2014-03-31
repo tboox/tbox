dd1cf4fe0d0f109a699b437aa49ad97c9c781adf
ruki <waruqi@gmail.com>
2014-03-31 10:02:41 +0800

    impl some asio ssl interfaces

--- src/asio/ssl.c
+++ src/asio/ssl.c
@@ -44,10 +44,178 @@
 // the aicp ssl type
 typedef struct __tb_aicp_ssl_t
 {
+	// the ssl 
+	tb_handle_t 			ssl;
+
+	// the aicp
+	tb_aicp_t* 				aicp;
+
+	// the aico
+	tb_handle_t 			aico;
+
+	// the sock
+	tb_handle_t 			sock;
+
+	// the timeout
+	tb_long_t 				timeout;
 
 }tb_aicp_ssl_t;
 
 /* ///////////////////////////////////////////////////////////////////////
  * implementation
  */
+tb_handle_t tb_aicp_ssl_init(tb_aicp_t* aicp, tb_bool_t bserver)
+{
+	// check
+	tb_assert_and_check_return_val(aicp, tb_null);
+
+	// done
+	tb_bool_t 		ok = tb_false;
+	tb_aicp_ssl_t* 	ssl = tb_null;
+	do
+	{
+		// make ssl
+		ssl = tb_malloc0(sizeof(tb_aicp_ssl_t));
+		tb_assert_and_check_break(ssl);
+
+		// init ssl
+		ssl->ssl = tb_ssl_init(bserver);
+		tb_assert_and_check_break(ssl->ssl);
+
+		// init aicp
+		ssl->aicp = aicp;
+
+		// ok
+		ok = tb_true;
+
+	} while (0);
+
+	// failed?
+	if (!ok)
+	{
+		// exit it
+		if (ssl) tb_aicp_ssl_exit(ssl, tb_false);
+		ssl = tb_null;
+	}
+
+	// ok?
+	return ssl;
+}
+tb_void_t tb_aicp_ssl_kill(tb_handle_t handle)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return(ssl);
+
+	// kill aico
+	if (ssl->aico) tb_aico_kill(ssl->aico);
+}
+tb_void_t tb_aicp_ssl_clos(tb_handle_t handle, tb_bool_t bcalling)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return(ssl);
+
+	// exit aico
+	if (ssl->aico) tb_aico_exit(ssl->aico, bcalling);
+	ssl->aico = tb_null;
+
+	// close ssl
+	if (ssl->ssl) tb_ssl_clos(ssl->ssl);
+}
+tb_void_t tb_aicp_ssl_exit(tb_handle_t handle, tb_bool_t bcalling)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return(ssl);
+
+	// close it first
+	tb_aicp_ssl_clos(ssl, bcalling);
+
+	// exit ssl
+	if (ssl->ssl) tb_ssl_exit(ssl->ssl);
+	ssl->ssl = tb_null;
+
+	// exit it
+	tb_free(ssl);
+}
+tb_void_t tb_aicp_ssl_set_sock(tb_handle_t handle, tb_handle_t sock)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return(ssl && sock);
+
+	// save sock
+	ssl->sock = sock;
+}
+tb_void_t tb_aicp_ssl_set_timeout(tb_handle_t handle, tb_long_t timeout)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return(ssl);
+
+	// save timeout
+	ssl->timeout = timeout;
+}
+tb_bool_t tb_aicp_ssl_open(tb_handle_t handle, tb_aicp_ssl_open_func_t func, tb_pointer_t priv)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return_val(ssl && func, tb_false);
+
+	// done
+	tb_bool_t ok = tb_false;
+	do
+	{
+		// check
+		tb_assert_and_check_break(ssl->aicp && ssl->ssl && ssl->sock && !ssl->aico);
+
+		// init aico
+		ssl->aico = tb_aico_init_sock(ssl->aicp, ssl->sock);
+		tb_assert_and_check_break(ssl->aico);
+
+		// init timeout
+		if (ssl->timeout)
+		{
+			tb_aico_timeout_set(ssl->aico, TB_AICO_TIMEOUT_RECV, ssl->timeout);
+			tb_aico_timeout_set(ssl->aico, TB_AICO_TIMEOUT_SEND, ssl->timeout);
+		}
+
+		// ok
+		ok = tb_true;
+
+	} while (0);
+
+	// ok?
+	return ok;
+}
+tb_bool_t tb_aicp_ssl_read(tb_handle_t handle, tb_size_t delay, tb_size_t maxn, tb_aicp_ssl_read_func_t func, tb_pointer_t priv)
+{
+	return tb_false;
+}
+tb_bool_t tb_aicp_ssl_writ(tb_handle_t handle, tb_size_t delay, tb_byte_t const* data, tb_size_t size, tb_aicp_ssl_writ_func_t func, tb_pointer_t priv)
+{
+	return tb_false;
+}
+tb_bool_t tb_aicp_ssl_task(tb_handle_t handle, tb_size_t delay, tb_aicp_ssl_task_func_t func, tb_pointer_t priv)
+{
+	return tb_false;
+}
+tb_bool_t tb_aicp_ssl_oread(tb_handle_t handle, tb_size_t maxn, tb_aicp_ssl_read_func_t func, tb_pointer_t priv)
+{
+	return tb_false;
+}
+tb_bool_t tb_aicp_ssl_owrit(tb_handle_t handle, tb_byte_t const* data, tb_size_t size, tb_aicp_ssl_writ_func_t func, tb_pointer_t priv)
+{
+	return tb_false;
+}
+tb_aicp_t* tb_aicp_ssl_aicp(tb_handle_t handle)
+{
+	// check
+	tb_aicp_ssl_t* ssl = (tb_aicp_ssl_t*)handle;
+	tb_assert_and_check_return_val(ssl, tb_null);
+
+	// the aicp
+	return ssl->aicp;
+}
 
--- src/asio/ssl.h
+++ src/asio/ssl.h
@@ -57,6 +57,19 @@ typedef tb_bool_t 	(*tb_aicp_ssl_open_func_t)(tb_handle_t ssl, tb_size_t state, 
  */
 typedef tb_bool_t 	(*tb_aicp_ssl_read_func_t)(tb_handle_t ssl, tb_size_t state, tb_byte_t const* data, tb_size_t real, tb_size_t size, tb_pointer_t priv);
 
+/*! the aicp ssl writ func type
+ *
+ * @param ssl 		the ssl handle
+ * @param state 	the state
+ * @param data 		the writed data
+ * @param real 		the real size, maybe zero
+ * @param size 		the need size
+ * @param priv 		the func private data
+ *
+ * @return 			tb_true: ok and continue it if need, tb_false: break it, but not break aicp
+ */
+typedef tb_bool_t 	(*tb_aicp_ssl_writ_func_t)(tb_handle_t ssl, tb_size_t state, tb_byte_t const* data, tb_size_t real, tb_size_t size, tb_pointer_t priv);
+
 /*! the aicp ssl task func type
  *
  * @param ssl 		the ssl handle
@@ -74,10 +87,11 @@ typedef tb_bool_t 	(*tb_aicp_ssl_task_func_t)(tb_handle_t ssl, tb_size_t state, 
 /*! init the ssl 
  *
  * @param aicp 		the aicp
+ * @param bserver 	is server endpoint?
  *
  * @return 			the ssl handle
  */
-tb_handle_t 		tb_aicp_ssl_init(tb_aicp_t* aicp);
+tb_handle_t 		tb_aicp_ssl_init(tb_aicp_t* aicp, tb_bool_t bserver);
 
 /*! kill the ssl
  *
@@ -98,6 +112,20 @@ tb_void_t 			tb_aicp_ssl_clos(tb_handle_t handle, tb_bool_t bcalling);
  * @param bcalling 	exit it at the self callback?
  */
 tb_void_t 			tb_aicp_ssl_exit(tb_handle_t handle, tb_bool_t bcalling);
+
+/*! set the ssl sock
+ * 
+ * @param handle 	the ssl handle
+ * @param sock 		the ssl sock
+ */
+tb_void_t 			tb_aicp_ssl_set_sock(tb_handle_t handle, tb_handle_t sock);
+
+/*! set the ssl timeout
+ * 
+ * @param handle 	the ssl handle
+ * @param timeout 	the ssl timeout, using the default timeout if be zero
+ */
+tb_void_t 			tb_aicp_ssl_set_timeout(tb_handle_t handle, tb_long_t timeout);
 
 /*! open the ssl 
  *
@@ -121,6 +149,19 @@ tb_bool_t 			tb_aicp_ssl_open(tb_handle_t handle, tb_aicp_ssl_open_func_t func, 
  */
 tb_bool_t 			tb_aicp_ssl_read(tb_handle_t handle, tb_size_t delay, tb_size_t maxn, tb_aicp_ssl_read_func_t func, tb_pointer_t priv);
 
+/*! writ the ssl after the delay time
+ *
+ * @param handle 	the ssl handle
+ * @param delay 	the delay time, ms
+ * @param data 		the data
+ * @param size 		the size
+ * @param func 		the func
+ * @param priv 		the func data
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_aicp_ssl_writ(tb_handle_t handle, tb_size_t delay, tb_byte_t const* data, tb_size_t size, tb_aicp_ssl_writ_func_t func, tb_pointer_t priv);
+
 /*! task the ssl
  *
  * @param handle 	the ssl handle
@@ -142,6 +183,18 @@ tb_bool_t 			tb_aicp_ssl_task(tb_handle_t handle, tb_size_t delay, tb_aicp_ssl_t
  * @return 			tb_true or tb_false
  */
 tb_bool_t 			tb_aicp_ssl_oread(tb_handle_t handle, tb_size_t maxn, tb_aicp_ssl_read_func_t func, tb_pointer_t priv);
+
+/*! open and writ the ssl, open it first if not opened 
+ *
+ * @param handle 	the ssl handle
+ * @param data 		the data
+ * @param size 		the size
+ * @param func 		the func
+ * @param priv 		the func data
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_aicp_ssl_owrit(tb_handle_t handle, tb_byte_t const* data, tb_size_t size, tb_aicp_ssl_writ_func_t func, tb_pointer_t priv);
 
 /*! the ssl aicp
  *
