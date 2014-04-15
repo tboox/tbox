e48ce8ffdb65a398c9d26c6d4971cfb0d3780847
ruki <waruqi@gmail.com>
2014-04-15 10:26:38 +0800

    add some thread pool interfaces

--- src/platform/thread_pool.h
+++ src/platform/thread_pool.h
@@ -30,17 +30,44 @@
 #include "prefix.h"
 
 /* ///////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+/// the thread pool task done func type
+typedef tb_void_t 						(*tb_thread_pool_task_done_func_t)(tb_pointer_t priv);
+
+/// the thread pool task exit func type
+typedef tb_void_t 						(*tb_thread_pool_task_exit_func_t)(tb_pointer_t priv);
+
+/// the thread pool task type
+typedef struct __tb_thread_pool_task_t
+{
+	/// the task done func
+	tb_thread_pool_task_done_func_t 	done;
+
+	/// the task exit func for freeing the private data
+	tb_thread_pool_task_exit_func_t 	exit;
+
+	/// the task private data
+	tb_pointer_t 						priv;
+
+	/// is urgent task?
+	tb_bool_t 							urgent;
+
+}tb_thread_pool_task_t;
+
+/* ///////////////////////////////////////////////////////////////////////
  * interfaces
  */
 
 /*! init thread pool
  *
- * @param maxn 			the thread max count
+ * @param worker_maxn 	the thread worker max count
  * @param stack 		the thread stack, using the default stack size if be zero 
  *
  * @return 				the thread pool handle
  */
-tb_handle_t 			tb_thread_pool_init(tb_size_t maxn, tb_size_t stack);
+tb_handle_t 			tb_thread_pool_init(tb_size_t worker_maxn, tb_size_t stack);
 
 /*! exit thread pool
  *
@@ -48,16 +75,90 @@ tb_handle_t 			tb_thread_pool_init(tb_size_t maxn, tb_size_t stack);
  */
 tb_void_t 				tb_thread_pool_exit(tb_handle_t pool);
 
-/*! kill thread pool
+/*! kill thread pool, all workers and waiting tasks 
  *
  * @param pool 			the thread pool handle
  */
 tb_void_t 				tb_thread_pool_kill(tb_handle_t pool);
+
+/*! the current worker count
+ *
+ * @param pool 			the thread pool handle
+ *
+ * @return 				the current worker count
+ */
+tb_size_t 				tb_thread_pool_worker_size(tb_handle_t pool);
+
+/*! the current waiting task count
+ *
+ * @param pool 			the thread pool handle
+ *
+ * @return 				the current task count
+ */
+tb_size_t 				tb_thread_pool_task_size(tb_handle_t pool);
+
+/*! post one task
+ *
+ * @param pool 			the thread pool handle
+ * @param done 			the task done func
+ * @param exit 			the task exit func, optional
+ * @param priv 			the task private data
+ * @param urgent 		is urgent task?
+ *
+ * @return 				the task handle
+ */
+tb_handle_t 			tb_thread_pool_task_post(tb_handle_t pool, tb_thread_pool_task_done_func_t done, tb_thread_pool_task_exit_func_t exit, tb_pointer_t priv, tb_bool_t urgent);
+
+/*! post task list
+ *
+ * @param pool 			the thread pool handle
+ * @param list 			the task list
+ * @param size 			the task count
+ * @param tasks 		the task handle list, optional
+ *
+ * @return 				tb_true or tb_false
+ */
+tb_bool_t 				tb_thread_pool_task_post_list(tb_handle_t pool, tb_thread_pool_task_t const* list, tb_size_t size, tb_handle_t* tasks);
+
+/*! kill one task
+ *
+ * @param pool 			the thread pool handle
+ * @param task 			the task handle
+ */
+tb_void_t 				tb_thread_pool_task_kill(tb_handle_t pool, tb_handle_t task);
+
+/*! kill task list
+ *
+ * @param pool 			the thread pool handle
+ * @param tasks 		the task handle list
+ * @param size 			the task handle count
+ */
+tb_void_t 				tb_thread_pool_task_kill_list(tb_handle_t pool, tb_handle_t* tasks, tb_size_t size);
+
+/*! kill all tasks
+ *
+ * @param pool 			the thread pool handle
+ */
+tb_void_t 				tb_thread_pool_task_kill_all(tb_handle_t pool);
 
 /*! the thread pool instance
  *
  * @param pool 			the thread pool handle
  */
 tb_handle_t 			tb_thread_pool_instance(tb_noarg_t);
+
+#ifdef __tb_debug__
+/*! dump all workers
+ *
+ * @param pool 			the thread pool handle
+ */
+tb_void_t 				tb_thread_pool_worker_dump(tb_handle_t pool);
+
+/*! dump all tasks
+ *
+ * @param pool 			the thread pool handle
+ */
+tb_void_t 				tb_thread_pool_task_dump(tb_handle_t pool);
+#endif
 
 #endif
