d01ed3c7cef5e5f61ede4da37fde6693d782e9f5
ruki <waruqi@gmail.com>
2014-07-09 11:23:31 +0800

    init static page pool init and exit

--- src/memory/impl/static_page_pool.c
+++ src/memory/impl/static_page_pool.c
@@ -18,30 +18,98 @@
  *
  * @author      ruki
  * @file        static_page_pool.c
- * @ingroup     memory
- *
  */
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * trace
  */
 #define TB_TRACE_MODULE_NAME            "static_page_pool"
-#define TB_TRACE_MODULE_DEBUG           (0)
+#define TB_TRACE_MODULE_DEBUG           (1)
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * includes
  */
 #include "static_page_pool.h"
 
+/*! the static page pool impl type
+ *
+ */
+typedef __tb_aligned__(TB_POOL_DATA_ALIGN) struct __tb_static_page_pool_impl_t
+{
+    // the data
+    tb_byte_t*                      data;
+
+    // the size
+    tb_size_t                       size;
+
+    // the page size
+    tb_size_t                       pagesize;
+
+#ifdef __tb_debug__
+
+    // the peak size
+    tb_size_t                       peak_size;
+
+    // the total size
+    tb_size_t                       total_size;
+
+    // the occupied size
+    tb_size_t                       occupied_size;
+
+    // the malloc count
+    tb_size_t                       malloc_count;
+
+    // the ralloc count
+    tb_size_t                       ralloc_count;
+
+    // the free count
+    tb_size_t                       free_count;
+#endif
+
+}__tb_aligned__(TB_POOL_DATA_ALIGN) tb_static_page_pool_impl_t;
+
 /* //////////////////////////////////////////////////////////////////////////////////////
- * the implementation
+ * implementation
  */
 tb_page_pool_ref_t tb_static_page_pool_init(tb_byte_t* data, tb_size_t size)
 {
-    return tb_null;
+    // check
+    tb_assert_and_check_return_val(data && size, tb_null);
+    tb_assert_static(!(sizeof(tb_static_page_pool_impl_t) & (TB_POOL_DATA_ALIGN - 1)));
+
+    // align data and size
+    tb_size_t diff = tb_align((tb_size_t)data, TB_POOL_DATA_ALIGN) - (tb_size_t)data;
+    tb_assert_and_check_return_val(size > diff + sizeof(tb_static_page_pool_impl_t), tb_null);
+    size -= diff;
+    data += diff;
+
+    // init pool
+    tb_static_page_pool_impl_t* impl = (tb_static_page_pool_impl_t*)data;
+    tb_memset(impl, 0, sizeof(tb_static_page_pool_impl_t));
+
+    // init pagesize
+    impl->pagesize = tb_page_size();
+    tb_assert_and_check_return_val(impl->pagesize, tb_null);
+
+    // init data 
+    impl->data = (tb_byte_t*)&impl[1];
+    tb_assert_and_check_return_val(!((tb_size_t)impl->data & (TB_POOL_DATA_ALIGN - 1)), tb_null);
+
+    // init size
+    impl->size = size - sizeof(tb_static_page_pool_impl_t);
+    tb_assert_and_check_return_val(impl->size > impl->pagesize, tb_null);
+
+    // ok
+    return (tb_page_pool_ref_t)impl;
 }
 tb_void_t tb_static_page_pool_exit(tb_page_pool_ref_t pool)
 {
+    // check
+    tb_static_page_pool_impl_t* impl = (tb_static_page_pool_impl_t*)pool;
+    tb_assert_and_check_return(impl);
+
+    // clear it
+    tb_static_page_pool_clear(pool);
 }
 tb_void_t tb_static_page_pool_clear(tb_page_pool_ref_t pool)
 {
@@ -61,5 +129,21 @@ tb_bool_t tb_static_page_pool_free(tb_page_pool_ref_t pool, tb_pointer_t data __
 #ifdef __tb_debug__
 tb_void_t tb_static_page_pool_dump(tb_page_pool_ref_t pool)
 {
+    // check
+    tb_static_page_pool_impl_t* impl = (tb_static_page_pool_impl_t*)pool;
+    tb_assert_and_check_return(impl);
+
+    // trace
+    tb_trace_i("======================================================================");
+
+    // trace debug info
+    tb_trace_i("peak_size: %lu",            impl->peak_size);
+    tb_trace_i("wast_rate: %llu/10000",     impl->occupied_size? (((tb_hize_t)impl->occupied_size - impl->total_size) * 10000) / (tb_hize_t)impl->occupied_size : 0);
+    tb_trace_i("free_count: %lu",           impl->free_count);
+    tb_trace_i("malloc_count: %lu",         impl->malloc_count);
+    tb_trace_i("ralloc_count: %lu",         impl->ralloc_count);
+
+    // trace
+    tb_trace_i("======================================================================");
 }
 #endif
