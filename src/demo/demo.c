f9cbdac75a43d3c81ef9de465397e1c72ed19230
ruki <waruqi@gmail.com>
2014-07-18 15:05:28 +0800

    fix to compute memory pool wast rate

--- src/demo/demo.c
+++ src/demo/demo.c
@@ -202,7 +202,7 @@ tb_int_t main(tb_int_t argc, tb_char_t** argv)
 {
     // init tbox
 #if 0
-    if (!tb_init(tb_null, (tb_byte_t*)malloc(500 * 1024 * 1024), 500 * 1024 * 1024)) return 0;
+    if (!tb_init(tb_null, (tb_byte_t*)malloc(300 * 1024 * 1024), 300 * 1024 * 1024)) return 0;
 #else
     if (!tb_init(tb_null, tb_null, 0)) return 0;
 #endif
