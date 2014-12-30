c8236edb1efb7c471008d187038e3d0b493cc2bc
ruki <waruqi@gmail.com>
2014-12-30 14:47:33 +0800

    add assert abort and check macros

--- src/tbox/prefix/assert.h
+++ src/tbox/prefix/assert.h
@@ -151,6 +151,7 @@ __tb_extern_c_enter__
 #   define tb_assert_break(x)                               { if (!(x)) {tb_trace_a("expr[%s]", #x); tb_assert_backtrace_dump(); break ; } }
 #   define tb_assert_continue(x)                            { if (!(x)) {tb_trace_a("expr[%s]", #x); tb_assert_backtrace_dump(); continue ; } }
 #   define tb_assert_break_state(x, s, v)                   { if (!(x)) { (s) = (v); tb_trace_a("expr[%s]", #x); tb_assert_backtrace_dump(); break ; } }
+
 #   define tb_assert_and_check_abort(x)                     tb_assert_abort(x)
 #   define tb_assert_and_check_return(x)                    tb_assert_return(x)
 #   define tb_assert_and_check_return_val(x, v)             tb_assert_return_val(x, v)
@@ -158,6 +159,13 @@ __tb_extern_c_enter__
 #   define tb_assert_and_check_break(x)                     tb_assert_break(x)
 #   define tb_assert_and_check_continue(x)                  tb_assert_continue(x)
 #   define tb_assert_and_check_break_state(x, s, v)         tb_assert_break_state(x, s, v)
+
+#   define tb_assert_abort_and_check_return(x)              tb_assert_abort(x)
+#   define tb_assert_abort_and_check_return_val(x, v)       tb_assert_abort(x)
+#   define tb_assert_abort_and_check_goto(x, b)             tb_assert_abort(x)
+#   define tb_assert_abort_and_check_break(x)               tb_assert_abort(x)
+#   define tb_assert_abort_and_check_continue(x)            tb_assert_abort(x)
+#   define tb_assert_abort_and_check_break_state(x, s, v)   tb_assert_abort(x)
 #else
 #   define tb_assert(x)                                     tb_assert_empty_impl()
 #   define tb_assert_abort(x)                               tb_assert_empty_impl()
@@ -167,6 +175,7 @@ __tb_extern_c_enter__
 #   define tb_assert_break(x)
 #   define tb_assert_continue(x)
 #   define tb_assert_break_state(x, s, v)
+
 #   define tb_assert_and_check_abort(x)                     tb_check_abort(x)
 #   define tb_assert_and_check_return(x)                    tb_check_return(x)
 #   define tb_assert_and_check_return_val(x, v)             tb_check_return_val(x, v)
@@ -174,6 +183,13 @@ __tb_extern_c_enter__
 #   define tb_assert_and_check_break(x)                     tb_check_break(x)
 #   define tb_assert_and_check_continue(x)                  tb_check_continue(x)
 #   define tb_assert_and_check_break_state(x, s, v)         tb_check_break_state(x, s, v)
+
+#   define tb_assert_abort_and_check_return(x)              tb_check_return(x)
+#   define tb_assert_abort_and_check_return_val(x, v)       tb_check_return_val(x, v)
+#   define tb_assert_abort_and_check_goto(x, b)             tb_check_goto(x, b)
+#   define tb_assert_abort_and_check_break(x)               tb_check_break(x)
+#   define tb_assert_abort_and_check_continue(x)            tb_check_continue(x)
+#   define tb_assert_abort_and_check_break_state(x, s, v)   tb_check_break_state(x, s, v)
 #endif
 
 #define tb_assert_static(x)                                 do { typedef int __tb_static_assert__[(x)? 1 : -1]; __tb_volatile__ __tb_static_assert__ __a; tb_used(__a); } while(0)
