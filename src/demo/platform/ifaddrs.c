dbee99fe57493e2612adff01ed2ffc5ef6e1866c
ruki <waruqi@gmail.com>
2015-01-09 17:37:22 +0800

    impl ifaddrs for posix

--- src/demo/platform/ifaddrs.c
+++ src/demo/platform/ifaddrs.c
@@ -15,7 +15,7 @@ tb_int_t tb_demo_platform_ifaddrs_main(tb_int_t argc, tb_char_t** argv)
 
     // dump hwaddr
     tb_hwaddr_t hwaddr;
-    if (tb_ifaddrs_hwaddr(tb_ifaddrs(), argv[1], &hwaddr))
+    if (tb_ifaddrs_hwaddr(tb_ifaddrs(), argv[1], tb_false, &hwaddr))
     {
         // trace
         tb_trace_i("name: %s, hwaddr: %{hwaddr}", argv[1], &hwaddr);
@@ -23,7 +23,7 @@ tb_int_t tb_demo_platform_ifaddrs_main(tb_int_t argc, tb_char_t** argv)
 
     // dump ipaddr4
     tb_ipaddr_t ipaddr4;
-    if (tb_ifaddrs_ipaddr(tb_ifaddrs(), argv[1], TB_IPADDR_FAMILY_IPV4, &ipaddr4))
+    if (tb_ifaddrs_ipaddr(tb_ifaddrs(), argv[1], tb_false, TB_IPADDR_FAMILY_IPV4, &ipaddr4))
     {
         // trace
         tb_trace_i("name: %s, ipaddr4: %{ipaddr}", argv[1], &ipaddr4);
@@ -31,7 +31,7 @@ tb_int_t tb_demo_platform_ifaddrs_main(tb_int_t argc, tb_char_t** argv)
 
     // dump ipaddr6
     tb_ipaddr_t ipaddr6;
-    if (tb_ifaddrs_ipaddr(tb_ifaddrs(), argv[1], TB_IPADDR_FAMILY_IPV6, &ipaddr6))
+    if (tb_ifaddrs_ipaddr(tb_ifaddrs(), argv[1], tb_false, TB_IPADDR_FAMILY_IPV6, &ipaddr6))
     {
         // trace
         tb_trace_i("name: %s, ipaddr6: %{ipaddr}", argv[1], &ipaddr6);
--- src/tbox/platform/ifaddrs.c
+++ src/tbox/platform/ifaddrs.c
@@ -47,6 +47,26 @@ static tb_void_t tb_ifaddrs_instance_exit(tb_handle_t ifaddrs, tb_cpointer_t pri
     // exit it
     tb_ifaddrs_exit((tb_ifaddrs_ref_t)ifaddrs);
 }
+static tb_long_t tb_ifaddrs_interface_comp(tb_iterator_ref_t iterator, tb_cpointer_t item, tb_cpointer_t name)
+{
+    // check
+    tb_assert_return_val(item, 0);
+
+    // comp
+    return tb_stricmp(((tb_ifaddrs_interface_ref_t)item)->name, (tb_char_t const*)name);
+}
+static tb_ifaddrs_interface_ref_t tb_ifaddrs_interface_find(tb_iterator_ref_t iterator, tb_char_t const* name)
+{
+    // check
+    tb_assert_and_check_return_val(iterator && name, tb_null);
+
+    // find it
+    tb_size_t itor = tb_find_all_if(iterator, tb_ifaddrs_interface_comp, name);
+    tb_check_return_val(itor != tb_iterator_tail(iterator), tb_null);
+
+    // ok
+    return (tb_ifaddrs_interface_ref_t)tb_iterator_item(iterator, itor);
+}
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
@@ -79,7 +99,22 @@ tb_ifaddrs_ref_t tb_ifaddrs()
 {
     return (tb_ifaddrs_ref_t)tb_singleton_instance(TB_SINGLETON_TYPE_IFADDRS, tb_ifaddrs_instance_init, tb_ifaddrs_instance_exit, tb_null);
 }
-tb_bool_t tb_ifaddrs_hwaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_hwaddr_ref_t hwaddr)
+tb_ifaddrs_interface_ref_t tb_ifaddrs_interface(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_bool_t reload)
+{
+    // check
+    tb_assert_and_check_return_val(ifaddrs && name, tb_null);
+
+    // the iterator
+    tb_iterator_ref_t iterator = tb_ifaddrs_itor(ifaddrs, reload);
+    tb_assert_and_check_return_val(iterator, tb_null);
+
+    // reload it if the cached interfaces is empty
+    if (!reload && !tb_iterator_size(iterator)) iterator = tb_ifaddrs_itor(ifaddrs, tb_true);
+
+    // ok
+    return tb_ifaddrs_interface_find(iterator, name);
+}
+tb_bool_t tb_ifaddrs_hwaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_bool_t reload, tb_hwaddr_ref_t hwaddr)
 {
     // check
     tb_assert_and_check_return_val(ifaddrs && hwaddr, tb_false);
@@ -87,56 +122,54 @@ tb_bool_t tb_ifaddrs_hwaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_
     // clear it first
     tb_hwaddr_clear(hwaddr);
 
+    // the iterator
+    tb_iterator_ref_t iterator = tb_ifaddrs_itor(ifaddrs, reload);
+    tb_assert_and_check_return_val(iterator, tb_false);
+
+    // reload it if the cached interfaces is empty
+    if (!reload && !tb_iterator_size(iterator)) iterator = tb_ifaddrs_itor(ifaddrs, tb_true);
+
     // done
-    tb_bool_t ok        = tb_false;
-    tb_bool_t reload    = tb_false;
-    do
+    tb_bool_t ok = tb_false;
+    tb_for_all_if (tb_ifaddrs_interface_ref_t, interface, iterator, interface)
     {
-        // attempt to get it first from the cached interfaces
-        tb_for_all_if (tb_ifaddrs_interface_ref_t, interface, tb_ifaddrs_itor(ifaddrs, reload), interface)
+        // get hwaddr from the given interface name?
+        if (name)
         {
-            // get hwaddr from the given interface name?
-            if (name)
+            // is this?
+            if (    (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_HWADDR)
+                &&  !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)
+                &&  (interface->name && !tb_strcmp(interface->name, name)))
             {
-                // is this?
-                if (    (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_HWADDR)
-                    &&  !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)
-                    &&  (interface->name && !tb_strcmp(interface->name, name)))
-                {
-                    // save hwaddr
-                    tb_hwaddr_copy(hwaddr, &interface->addr.hw);
+                // save hwaddr
+                tb_hwaddr_copy(hwaddr, &interface->hwaddr);
 
-                    // ok
-                    ok = tb_true;
-                    break;
-                }
-            }
-            else
-            {
-                // is this?
-                if (    (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_HWADDR)
-                    &&  (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_IPADDR)
-                    &&  !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK))
-                {
-                    // save hwaddr
-                    tb_hwaddr_copy(hwaddr, &interface->addr.hw);
-
-                    // ok
-                    ok = tb_true;
-                    break;
-                }
+                // ok
+                ok = tb_true;
+                break;
             }
         }
+        else
+        {
+            // is this?
+            if (    (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_HWADDR)
+                &&  (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR)
+                &&  !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK))
+            {
+                // save hwaddr
+                tb_hwaddr_copy(hwaddr, &interface->hwaddr);
 
-        // attempt to get it again after reloading interfaces if be failed
-        reload = !reload;
-
-    } while (!ok && reload);
+                // ok
+                ok = tb_true;
+                break;
+            }
+        }
+    }
 
     // ok?
     return ok;
 }
-tb_bool_t tb_ifaddrs_ipaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_size_t family, tb_ipaddr_ref_t ipaddr)
+tb_bool_t tb_ifaddrs_ipaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_bool_t reload, tb_size_t family, tb_ipaddr_ref_t ipaddr)
 {
     // check
     tb_assert_and_check_return_val(ifaddrs && ipaddr, tb_false);
@@ -144,33 +177,51 @@ tb_bool_t tb_ifaddrs_ipaddr(tb_ifaddrs_ref_t ifaddrs, tb_char_t const* name, tb_
     // clear it first
     tb_ipaddr_clear(ipaddr);
 
+    // the iterator
+    tb_iterator_ref_t iterator = tb_ifaddrs_itor(ifaddrs, reload);
+    tb_assert_and_check_return_val(iterator, tb_false);
+
+    // reload it if the cached interfaces is empty
+    if (!reload && !tb_iterator_size(iterator)) iterator = tb_ifaddrs_itor(ifaddrs, tb_true);
+
+    // the ipaddr flags
+    tb_uint32_t ipflags = 0;
+    if (family == TB_IPADDR_FAMILY_IPV4) ipflags |= TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR4;
+    else if (family == TB_IPADDR_FAMILY_IPV6) ipflags |= TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR6;
+
     // done
-    tb_bool_t ok        = tb_false;
-    tb_bool_t reload    = tb_false;
-    do
+    tb_bool_t ok = tb_false;
+    tb_for_all_if (tb_ifaddrs_interface_ref_t, interface, iterator, interface)
     {
-        // attempt to get it first from the cached interfaces
-        tb_for_all_if (tb_ifaddrs_interface_ref_t, interface, tb_ifaddrs_itor(ifaddrs, reload), interface)
+        // is this?
+        if (    !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)
+            &&  (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR)
+            &&  (!name || (interface->name && !tb_strcmp(interface->name, name))))
         {
-            // is this?
-            if (    (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_IPADDR)
-                &&  !(interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)
-                &&  (!family || tb_ipaddr_family(&interface->addr.ip) == family)
-                &&  (!name || (interface->name && !tb_strcmp(interface->name, name))))
+            // ipv4?
+            if (    interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR4
+                &&  (!family || family == TB_IPADDR_FAMILY_IPV4))
             {
-                // save ipaddr
-                tb_ipaddr_copy(ipaddr, &interface->addr.ip);
+                // save ipaddr4
+                tb_ipaddr_ipv4_set(ipaddr, &interface->ipaddr4);
+
+                // ok
+                ok = tb_true;
+                break;
+            }
+            // ipv6?
+            else if (    interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR6
+                    &&  (!family || family == TB_IPADDR_FAMILY_IPV6))
+            {
+                // save ipaddr6
+                tb_ipaddr_ipv6_set(ipaddr, &interface->ipaddr6);
 
                 // ok
                 ok = tb_true;
                 break;
             }
         }
-
-        // attempt to get it again after reloading interfaces if be failed
-        reload = !reload;
-
-    } while (!ok && reload);
+    }
 
     // ok?
     return ok;
@@ -185,11 +236,13 @@ tb_void_t tb_ifaddrs_dump(tb_ifaddrs_ref_t ifaddrs)
     tb_for_all_if (tb_ifaddrs_interface_ref_t, interface, tb_ifaddrs_itor(ifaddrs, tb_true), interface)
     {
         // trace
-        tb_trace_i("name: %s%s",                  interface->name, (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)? "[loopback]" : "");
-        if (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_IPADDR)
-            tb_trace_i("    ipaddr: %{ipaddr}",     &interface->addr.ip);
-        if (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_HWADDR)
-            tb_trace_i("    hwaddr: %{hwaddr}",     &interface->addr.hw);
+        tb_trace_i("name: %s%s", interface->name, (interface->flags & TB_IFADDRS_INTERFACE_FLAG_IS_LOOPBACK)? "[loopback]" : "");
+        if (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR4)
+            tb_trace_i("    ipaddr4: %{ipv4}",  &interface->ipaddr4);
+        if (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_IPADDR6)
+            tb_trace_i("    ipaddr6: %{ipv6}",  &interface->ipaddr6);
+        if (interface->flags & TB_IFADDRS_INTERFACE_FLAG_HAVE_HWADDR)
+            tb_trace_i("    hwaddr: %{hwaddr}", &interface->hwaddr);
     }
 }
 #endif
