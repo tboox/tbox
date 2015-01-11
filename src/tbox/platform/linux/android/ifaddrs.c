c565d332ef46a4145966898214d4df31b1a41d03
ruki <waruqi@gmail.com>
2015-01-11 16:14:50 +0800

    add some codes for android ifaddrs

--- src/tbox/platform/linux/android/ifaddrs.c
+++ src/tbox/platform/linux/android/ifaddrs.c
@@ -25,21 +25,62 @@
  * includes
  */
 #include "prefix.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netpacket/packet.h>
+#include <net/if_arp.h>
+#include <netinet/in.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+#include "../../posix/sockaddr.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * private implementation
+ */
+static tb_void_t tb_ifaddrs_interface_exit(tb_item_func_t* func, tb_pointer_t buff)
+{
+    // check
+    tb_ifaddrs_interface_ref_t interface = (tb_ifaddrs_interface_ref_t)buff;
+    if (interface)
+    {
+        // exit the interface name
+        if (interface->name) tb_free(interface->name);
+        interface->name = tb_null;
+    }
+}
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
 tb_ifaddrs_ref_t tb_ifaddrs_init()
 {
-    tb_trace_noimpl();
-    return tb_null;
+    // init it
+    return (tb_ifaddrs_ref_t)tb_list_init(8, tb_item_func_mem(sizeof(tb_ifaddrs_interface_t), tb_ifaddrs_interface_exit, tb_null));
 }
 tb_void_t tb_ifaddrs_exit(tb_ifaddrs_ref_t ifaddrs)
 {
-    tb_trace_noimpl();
+    // exit it
+    if (ifaddrs) tb_list_exit((tb_list_ref_t)ifaddrs);
 }
 tb_iterator_ref_t tb_ifaddrs_itor(tb_ifaddrs_ref_t ifaddrs, tb_bool_t reload)
 {
+    // check
+    tb_list_ref_t interfaces = (tb_list_ref_t)ifaddrs;
+    tb_assert_and_check_return_val(interfaces, tb_null);
+
+    // uses the cached interfaces?
+    tb_check_return_val(reload, (tb_iterator_ref_t)interfaces); 
+
+    // clear interfaces first
+    tb_list_clear(interfaces);
+
+    // TODO
     tb_trace_noimpl();
-    return tb_null;
+
+    // ok?
+    return (tb_iterator_ref_t)interfaces;
 }
