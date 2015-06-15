73964b5bd625f468337b258291a8d11f04bca000
ruki <waruqi@gmail.com>
2015-06-15 22:47:38 +0800

    compile ok for macosx

--- src/demo/xmake.lua
+++ src/demo/xmake.lua
@@ -25,4 +25,65 @@ add_target("demo")
     add_links("tbox")
 
     -- add the source files
-    add_files("src/demo/**.c") 
+    add_files("src/demo/*.c") 
+    add_files("src/demo/libc/*.c") 
+    add_files("src/demo/libm/integer.c") 
+    add_files("src/demo/math/random.c") 
+    add_files("src/demo/utils/url.c") 
+    add_files("src/demo/utils/crc.c") 
+    add_files("src/demo/utils/md5.c") 
+    add_files("src/demo/utils/sha.c") 
+    add_files("src/demo/utils/bits.c") 
+    add_files("src/demo/utils/dump.c") 
+    add_files("src/demo/utils/base*.c") 
+    add_files("src/demo/other/test.c") 
+    add_files("src/demo/other/flv.c") 
+    add_files("src/demo/other/libflv.c") 
+    add_files("src/demo/string/*.c") 
+    add_files("src/demo/memory/**.c") 
+    add_files("src/demo/network/**.c") 
+    add_files("src/demo/platform/*.c") 
+    add_files("src/demo/container/*.c") 
+    add_files("src/demo/algorithm/*.c") 
+    add_files("src/demo/stream/stream.c") 
+    add_files("src/demo/stream/stream/*.c") 
+
+    -- add the source files for the float type
+    if options("float") then
+        add_files("src/demo/math/fixed.c")
+        add_files("src/demo/libm/float.c")
+        add_files("src/demo/libm/double.c")
+    end
+
+    -- add the source files for the xml module
+    if options("xml") then
+        add_files("src/demo/xml/*.c")
+    end
+
+    -- add the source files for the asio module
+    if options("asio") then
+        add_files("src/demo/asio/*.c")
+        add_files("src/demo/stream/async_stream.c")
+        add_files("src/demo/stream/transfer_pool.c")
+        add_files("src/demo/stream/async_transfer.c")
+        add_files("src/demo/stream/async_stream/*.c")
+    end
+
+    -- add the source files for the object module
+    if options("object") then
+        add_files("src/demo/utils/option.c")
+        add_files("src/demo/object/jcat.c")
+        add_files("src/demo/object/json.c")
+        add_files("src/demo/object/bin.c")
+        add_files("src/demo/object/xml.c")
+        add_files("src/demo/object/xplist.c")
+        add_files("src/demo/object/bplist.c")
+        add_files("src/demo/object/dump.c")
+    end
+
+    -- add the source files for the charset module
+    if options("charset") then add_files("src/demo/other/charset.c") end
+
+    -- add the source files for the database module
+    if options("database") then add_files("src/demo/database/sql.c") end
+    
