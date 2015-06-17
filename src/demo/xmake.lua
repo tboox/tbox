2dd3da924d4da1dba659bc456bc24e7bf654939f
ruki <waruqi@gmail.com>
2015-06-17 21:07:38 +0800

    using the relative pathes for xmake.lua

--- src/demo/xmake.lua
+++ src/demo/xmake.lua
@@ -34,51 +34,51 @@ add_target("demo")
     add_options(libc, libm, posix, systemv)
 
     -- add the source files
-    add_files("src/demo/*.c") 
-    add_files("src/demo/libc/*.c") 
-    add_files("src/demo/libm/integer.c") 
-    add_files("src/demo/math/random.c") 
-    add_files("src/demo/utils/*.c|option.c") 
-    add_files("src/demo/other/*.c|charset.c") 
-    add_files("src/demo/string/*.c") 
-    add_files("src/demo/memory/**.c") 
-    add_files("src/demo/network/**.c") 
-    add_files("src/demo/platform/*.c") 
-    add_files("src/demo/container/*.c") 
-    add_files("src/demo/algorithm/*.c") 
-    add_files("src/demo/stream/stream.c") 
-    add_files("src/demo/stream/stream/*.c") 
+    add_files("*.c") 
+    add_files("libc/*.c") 
+    add_files("libm/integer.c") 
+    add_files("math/random.c") 
+    add_files("utils/*.c|option.c") 
+    add_files("other/*.c|charset.c") 
+    add_files("string/*.c") 
+    add_files("memory/**.c") 
+    add_files("network/**.c") 
+    add_files("platform/*.c") 
+    add_files("container/*.c") 
+    add_files("algorithm/*.c") 
+    add_files("stream/stream.c") 
+    add_files("stream/stream/*.c") 
 
     -- add the source files for the float type
     if options("float") then
-        add_files("src/demo/math/fixed.c")
-        add_files("src/demo/libm/float.c")
-        add_files("src/demo/libm/double.c")
+        add_files("math/fixed.c")
+        add_files("libm/float.c")
+        add_files("libm/double.c")
     end
 
     -- add the source files for the xml module
     if options("xml") then
-        add_files("src/demo/xml/*.c")
+        add_files("xml/*.c")
     end
 
     -- add the source files for the asio module
     if options("asio") then
-        add_files("src/demo/asio/*.c")
-        add_files("src/demo/stream/async_stream.c")
-        add_files("src/demo/stream/transfer_pool.c")
-        add_files("src/demo/stream/async_transfer.c")
-        add_files("src/demo/stream/async_stream/*.c")
+        add_files("asio/*.c")
+        add_files("stream/async_stream.c")
+        add_files("stream/transfer_pool.c")
+        add_files("stream/async_transfer.c")
+        add_files("stream/async_stream/*.c")
     end
 
     -- add the source files for the object module
     if options("object") then
-        add_files("src/demo/utils/option.c")
-        add_files("src/demo/object/*.c")
+        add_files("utils/option.c")
+        add_files("object/*.c")
     end
 
     -- add the source files for the charset module
-    if options("charset") then add_files("src/demo/other/charset.c") end
+    if options("charset") then add_files("other/charset.c") end
 
     -- add the source files for the database module
-    if options("database") then add_files("src/demo/database/sql.c") end
+    if options("database") then add_files("database/sql.c") end
     
