c096465d30e66e18f1c25ea463c8f7ce5fe8153a
Olexander Yermakov <mannavard1611@gmail.com>
2016-01-29 13:56:13 +0200

    utils: implement fnv32 hash

---
+++ src/demo/utils/fnv32.c
@@ -0,0 +1,17 @@
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "../demo.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * main
+ */
+tb_int_t tb_demo_utils_fnv32_main(tb_int_t argc, tb_char_t** argv)
+{
+    // data
+    tb_byte_t*  data = (tb_byte_t*)argv[1];
+
+    // trace
+    tb_printf("[fnv32]: %x\n", tb_fnv32_encode(data));
+    return 0;
+}
