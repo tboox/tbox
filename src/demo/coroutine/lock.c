cfb370674371995d5aa27c6828f9d4a8ec856426
ruki <waruqi@gmail.com>
2016-10-17 17:22:44 +0800

    impl coroutine lock demo

--- src/demo/coroutine/lock.c
+++ src/demo/coroutine/lock.c
@@ -4,79 +4,32 @@
 #include "../demo.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
- * macros
- */
-
-// the switch count
-#define COUNT       (10000000)
-
-/* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */ 
 static tb_void_t tb_demo_coroutine_lock_test_func(tb_cpointer_t priv)
 {
+    // check
+    tb_co_lock_ref_t lock = (tb_co_lock_ref_t)priv;
+    tb_assert_and_check_return(lock);
+
     // loop
-    tb_size_t count = (tb_size_t)priv;
+    tb_size_t count = 10;
     while (count--)
     {
-        // trace
-        tb_trace_i("[coroutine: %p]: %lu", tb_coroutine_self(), count);
-
-        // yield
-        tb_coroutine_yield();
-    }
-}
-static tb_void_t tb_demo_coroutine_lock_test()
-{
-    // init scheduler
-    tb_co_scheduler_ref_t scheduler = tb_co_scheduler_init();
-    if (scheduler)
-    {
-        // start coroutines
-        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, (tb_cpointer_t)10, 0);
-        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, (tb_cpointer_t)10, 0);
-
-        // run scheduler
-        tb_co_scheduler_loop(scheduler);
-
-        // exit scheduler
-        tb_co_scheduler_exit(scheduler);
-    }
-}
-static tb_void_t tb_demo_coroutine_lock_pref_func(tb_cpointer_t priv)
-{
-    // loop
-    tb_size_t count = (tb_size_t)priv;
-    while (count--)
-    {
-        // yield
-        tb_coroutine_yield();
-    }
-}
-static tb_void_t tb_demo_coroutine_lock_pref()
-{
-    // init scheduler
-    tb_co_scheduler_ref_t scheduler = tb_co_scheduler_init();
-    if (scheduler)
-    {
-        // start coroutine
-        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_pref_func, (tb_cpointer_t)(COUNT >> 1), 0);
-        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_pref_func, (tb_cpointer_t)(COUNT >> 1), 0);
-
-        // init the start time
-        tb_hong_t startime = tb_mclock();
-
-        // run scheduler
-        tb_co_scheduler_loop(scheduler);
-
-        // computing time
-        tb_hong_t duration = tb_mclock() - startime;
+        // enter lock
+        tb_co_lock_enter(lock);
 
         // trace
-        tb_trace_i("%d switches in %lld ms, %lld switches per second", COUNT, duration, (((tb_hong_t)1000 * COUNT) / duration));
+        tb_trace_i("[coroutine: %p]: enter", tb_coroutine_self());
 
-        // exit scheduler
-        tb_co_scheduler_exit(scheduler);
+        // wait some time
+        tb_msleep(1000);
+
+        // trace
+        tb_trace_i("[coroutine: %p]: leave", tb_coroutine_self());
+
+        // leave lock
+        tb_co_lock_leave(lock);
     }
 }
 
@@ -85,7 +38,29 @@ static tb_void_t tb_demo_coroutine_lock_pref()
  */ 
 tb_int_t tb_demo_coroutine_lock_main(tb_int_t argc, tb_char_t** argv)
 {
-    tb_demo_coroutine_lock_test();
-    tb_demo_coroutine_lock_pref();
+    // init scheduler
+    tb_co_scheduler_ref_t scheduler = tb_co_scheduler_init();
+    if (scheduler)
+    {
+        // init lock
+        tb_co_lock_ref_t lock = tb_co_lock_init();
+        tb_assert(lock);
+        
+        // start coroutines
+        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, lock, 0);
+        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, lock, 0);
+        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, lock, 0);
+        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, lock, 0);
+        tb_coroutine_start(scheduler, tb_demo_coroutine_lock_test_func, lock, 0);
+
+        // exit lock
+        tb_co_lock_exit(lock);
+
+        // run scheduler
+        tb_co_scheduler_loop(scheduler);
+
+        // exit scheduler
+        tb_co_scheduler_exit(scheduler);
+    }
     return 0;
 }
