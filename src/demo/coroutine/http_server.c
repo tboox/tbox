a0cdbe7451f2fbd7ee6b76788d902d8c57277329
ruki <waruqi@gmail.com>
2016-10-27 10:35:15 +0800

    modify default cpu core count for http server demo

--- src/demo/coroutine/http_server.c
+++ src/demo/coroutine/http_server.c
@@ -14,7 +14,7 @@
 #define TB_DEMO_TIMEOUT     (-1)
 
 // the cpu-core count
-#define TB_DEMO_CPU         (1)
+#define TB_DEMO_CPU         (4)
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * types
