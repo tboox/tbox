4d90dbcd424b4339a3fa6541167f62ce822067dc
ruki <waruqi@gmail.com>
2019-07-31 00:23:02 +0800

    add atomic_compare_and_set

--- src/tbox/platform/atomic.h
+++ src/tbox/platform/atomic.h
@@ -38,127 +38,151 @@
  * macros
  */
 
-/*!initializes the default-constructed atomic object obj with the value desired. 
+/*! initializes the default-constructed atomic object obj with the value desired. 
  *
  * the function is not atomic: concurrent access from another thread, even through an atomic operation, is a data race.
  */
 #ifndef tb_atomic_init
-#   define tb_atomic_init(a, v)               do { *(a) = (v); } while (0)
+#   define tb_atomic_init(a, v)                         do { *(a) = (v); } while (0)
 #endif
 
+/*! atomically compares the contents of memory pointed to by obj with the contents of memory pointed to by expected, 
+ * and if those are bitwise equal, replaces the former with desired (performs read-modify-write operation).
+ *
+ * otherwise, loads the actual contents of memory pointed to by obj into *p (performs load operation).
+ *
+ * @param a     pointer to the atomic object to test and modify
+ * @param p     pointer to the value expected to be found in the atomic object
+ * @param v     the value to store in the atomic object if it is as expected
+ *
+ * @return the result of the comparison: true if *a was equal to *p, false otherwise.
+ * - succ	    the memory synchronization ordering for the read-modify-write operation if the comparison succeeds. All values are permitted.
+ * - fail	    the memory synchronization ordering for the load operation if the comparison fails. Cannot be memory_order_release or memory_order_acq_rel and cannot specify stronger ordering than succ
+ *
+ * @code
+ *   tb_atomic_init(&a, 1);
+ *
+ *   tb_long_t expected = 1;
+ *   if (tb_atomic_compare_and_set(&a, &expected, 2)) {
+ *      // *a = 2
+ *   } else {
+ *      // expected = *a
+ *   }
+ * @endcode
+ */
+#ifndef tb_atomic_compare_and_set
+#   define tb_atomic_compare_and_set(a, p, v)           tb_atomic_compare_and_set_generic(a, p, v)
+#endif
+
+/*! like tb_atomic_compare_and_set(), but it's allowed to fail spuriously, that is, act as if *obj != *p even if they are equal. 
+ *
+ * when a compare-and-exchange is in a loop, the weak version will yield better performance on some platforms. 
+ * when a weak compare-and-exchange would require a loop and a strong one would not, the strong one is preferable.
+ *
+ * @param a     pointer to the atomic object to test and modify
+ * @param p     pointer to the value expected to be found in the atomic object
+ * @param v     the value to store in the atomic object if it is as expected
+ *
+ * @return the result of the comparison: true if *a was equal to *p, false otherwise.
+ * - succ	    the memory synchronization ordering for the read-modify-write operation if the comparison succeeds. All values are permitted.
+ * - fail	    the memory synchronization ordering for the load operation if the comparison fails. Cannot be memory_order_release or memory_order_acq_rel and cannot specify stronger ordering than succ
+ */
+#ifndef tb_atomic_compare_and_set_weak
+#   define tb_atomic_compare_and_set_weak(a, p, v)      tb_atomic_compare_and_set_generic(a, p, v)
+#endif
+
+// @deprecated
 #ifndef tb_atomic_fetch_and_pset
-#   define tb_atomic_fetch_and_pset(a, p, v)  tb_atomic_fetch_and_pset_generic(a, p, v)
+#   define tb_atomic_fetch_and_pset(a, p, v)            tb_atomic_fetch_and_pset_generic(a, p, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_set
-#   define tb_atomic_fetch_and_set(a, v)      tb_atomic_fetch_and_set_generic(a, v)
+#   define tb_atomic_fetch_and_set(a, v)                tb_atomic_fetch_and_set_generic(a, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_add
-#   define tb_atomic_fetch_and_add(a, v)      tb_atomic_fetch_and_add_generic(a, v)
+#   define tb_atomic_fetch_and_add(a, v)                tb_atomic_fetch_and_add_generic(a, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_or
-#   define tb_atomic_fetch_and_or(a, v)       tb_atomic_fetch_and_or_generic(a, v)
+#   define tb_atomic_fetch_and_or(a, v)                 tb_atomic_fetch_and_or_generic(a, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_xor
-#   define tb_atomic_fetch_and_xor(a, v)      tb_atomic_fetch_and_xor_generic(a, v)
+#   define tb_atomic_fetch_and_xor(a, v)                tb_atomic_fetch_and_xor_generic(a, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_and
-#   define tb_atomic_fetch_and_and(a, v)      tb_atomic_fetch_and_and_generic(a, v)
+#   define tb_atomic_fetch_and_and(a, v)                tb_atomic_fetch_and_and_generic(a, v)
 #endif
 
 #ifndef tb_atomic_get
-#   define tb_atomic_get(a)                   tb_atomic_fetch_and_pset(a, 0, 0)
+#   define tb_atomic_get(a)                             tb_atomic_get_generic(a)
 #endif
 
 #ifndef tb_atomic_set
-#   define tb_atomic_set(a, v)                tb_atomic_fetch_and_set(a, v)
+#   define tb_atomic_set(a, v)                          tb_atomic_fetch_and_set(a, v)
 #endif
 
 #ifndef tb_atomic_fetch_and_sub
-#   define tb_atomic_fetch_and_sub(a, v)      tb_atomic_fetch_and_add(a, -(v))
+#   define tb_atomic_fetch_and_sub(a, v)                tb_atomic_fetch_and_add(a, -(v))
 #endif
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * inlines
  */
-static __tb_inline__ tb_long_t tb_atomic_fetch_and_pset_generic(tb_atomic_t* a, tb_long_t p, tb_long_t v)
+static __tb_inline__ tb_bool_t tb_atomic_compare_and_set_generic(tb_atomic_t* a, tb_long_t* p, tb_long_t v)
 {
     // FIXME
     // no safe
 
-    // done
-    tb_long_t o = *a; if (o == p) *a = v;
-
-    // ok
-    return o;
+    tb_atomic_t o = *a;
+    if (o == *p) 
+    {
+        *a = v;
+        return tb_true;
+    }
+    else
+    {
+        *p = o;
+        return tb_false;
+    }
+}
+static __tb_inline__ tb_long_t tb_atomic_fetch_and_pset_generic(tb_atomic_t* a, tb_long_t p, tb_long_t v)
+{
+    tb_atomic_compare_and_set(a, &p, v);
+    return p;
+}
+static __tb_inline__ tb_long_t tb_atomic_get_generic(tb_atomic_t* a)
+{
+    tb_long_t p = 0;
+    tb_atomic_compare_and_set(a, &p, 0);
+    return p;
 }
 static __tb_inline__ tb_long_t tb_atomic_fetch_and_set_generic(tb_atomic_t* a, tb_long_t v)
 {
-    // done
     tb_long_t o;
-    do
-    {
-        o = *a;
-
-    } while (tb_atomic_fetch_and_pset(a, o, v) != o);
-
-    // ok
+    do { o = *a; } while (!tb_atomic_compare_and_set_weak(a, &o, v));
     return o;
 }
 static __tb_inline__ tb_long_t tb_atomic_fetch_and_add_generic(tb_atomic_t* a, tb_long_t v)
 {
-    // done
-    tb_long_t o;
-    do
-    {
-        o = *a;
-
-    } while (tb_atomic_fetch_and_pset(a, o, o + v) != o);
-
-    // ok
+    tb_long_t o; do { o = *a; } while (!tb_atomic_compare_and_set_weak(a, &o, o + v));
     return o;
 }
 static __tb_inline__ tb_long_t tb_atomic_fetch_and_xor_generic(tb_atomic_t* a, tb_long_t v)
 {
-    // done
-    tb_long_t o;
-    do
-    {
-        o = *a;
-
-    } while (tb_atomic_fetch_and_pset(a, o, o ^ v) != o);
-
-    // ok
+    tb_long_t o; do { o = *a; } while (!tb_atomic_compare_and_set_weak(a, &o, o ^ v));
     return o;
 }
 static __tb_inline__ tb_long_t tb_atomic_fetch_and_and_generic(tb_atomic_t* a, tb_long_t v)
 {
-    // done
-    tb_long_t o;
-    do
-    {
-        o = *a;
-
-    } while (tb_atomic_fetch_and_pset(a, o, o & v) != o);
-
-    // ok
+    tb_long_t o; do { o = *a; } while (!tb_atomic_compare_and_set_weak(a, &o, o & v));
     return o;
 }
 static __tb_inline__ tb_long_t tb_atomic_fetch_and_or_generic(tb_atomic_t* a, tb_long_t v)
 {
-    // done
-    tb_long_t o;
-    do
-    {
-        o = *a;
-
-    } while (tb_atomic_fetch_and_pset(a, o, o | v) != o);
-
-    // ok
+    tb_long_t o; do { o = *a; } while (!tb_atomic_compare_and_set_weak(a, &o, o | v));
     return o;
 }
 
