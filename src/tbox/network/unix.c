c9064d5ec6fb94616857475d08fc95ead4eafa8c
CodeHz <CodeHz@outlook.com>
2019-12-16 11:43:28 +0800

    Add basic unix type

---
+++ src/tbox/network/unix.c
@@ -0,0 +1,82 @@
+/*!The Treasure Box Library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Copyright (C) 2009 - 2019, TBOOX Open Source Group.
+ *
+ * @author      ruki
+ * @file        ipv4.c 
+ * @ingroup     network
+ *
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * trace
+ */
+#define TB_TRACE_MODULE_NAME            "ipv4"
+#define TB_TRACE_MODULE_DEBUG           (0)
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "unix.h"
+#include "../libc/libc.h"
+#include "../math/math.h"
+#include "../utils/utils.h"
+#include "../string/string.h"
+#include "../platform/platform.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * interfaces
+ */
+tb_void_t tb_unix_clear(tb_unix_ref_t unix)
+{
+    // check
+    tb_assert_and_check_return(unix);
+
+    // clear it
+    tb_memset(unix->str, 0, sizeof(unix->str));
+}
+tb_bool_t tb_unix_is_equal(tb_unix_ref_t unix, tb_unix_ref_t other)
+{
+    // check
+    tb_assert_and_check_return_val(unix && other, tb_false);
+
+    // is equal?
+    return tb_strcmp(unix->str, other->str) == 0;
+}
+tb_char_t const* tb_unix_cstr(tb_unix_ref_t unix, tb_char_t* data, tb_size_t maxn)
+{
+    // check
+    tb_assert_and_check_return_val(unix && data && maxn >= TB_UNIX_CSTR_MAXN, tb_null);
+
+    // make it
+    tb_long_t size = tb_snprintf(data, maxn - 1, "%s", unix->str);
+    if (size >= 0) data[size] = '\0';
+
+    // ok
+    return data;
+}
+tb_bool_t tb_unix_cstr_set(tb_unix_ref_t unix, tb_char_t const* cstr)
+{
+    // check
+    tb_assert_and_check_return_val(cstr, tb_false);
+    tb_size_t len = tb_strlen(cstr);
+    tb_assert_and_check_return_val(len < TB_UNIX_CSTR_MAXN, tb_false);
+
+    // copy
+    tb_memcpy(unix->str, cstr, len + 1);
+
+    // ok
+    return tb_true;
+}
---
+++ src/tbox/network/unix.h
@@ -0,0 +1,100 @@
+/*!The Treasure Box Library
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Copyright (C) 2009 - 2019, TBOOX Open Source Group.
+ *
+ * @author      CodeHz
+ * @file        unix.h
+ * @ingroup     network
+ *
+ */
+#ifndef TB_NETWORK_UNIX_H
+#define TB_NETWORK_UNIX_H
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "prefix.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * extern
+ */
+__tb_extern_c_enter__
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * macros
+ */
+
+/// the ipv4 string data maxn
+#define TB_UNIX_CSTR_MAXN           (108)
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+/*! the ipv4 type
+ *
+ * xxx.xxx.xxx.xxx
+ */
+typedef union __tb_unix_t
+{
+    /// str
+    tb_char_t      str[TB_UNIX_CSTR_MAXN];
+
+}tb_unix_t, *tb_unix_ref_t;
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * interfaces
+ */
+
+/*! clear the unix
+ *
+ * @param unix      the unix
+ */
+tb_void_t           tb_unix_clear(tb_unix_ref_t unix);
+
+/*! is equal?
+ *
+ * @param unix      the unix
+ * @param other     the other unix
+ *
+ * @return          tb_true or tb_false
+ */
+tb_bool_t           tb_unix_is_equal(tb_unix_ref_t unix, tb_unix_ref_t other);
+
+/*! get the unix string
+ *
+ * @param unix      the unix
+ * @param data      the unix data
+ * @param maxn      the data maxn
+ *
+ * @return          the unix address
+ */
+tb_char_t const*    tb_unix_cstr(tb_unix_ref_t unix, tb_char_t* data, tb_size_t maxn);
+
+/*! set the unix from string
+ *
+ * @param unix      the unix
+ * @param cstr      the unix string 
+ *
+ * @return          tb_true or tb_false
+ */
+tb_bool_t           tb_unix_cstr_set(tb_unix_ref_t unix, tb_char_t const* cstr);
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * extern
+ */
+__tb_extern_c_leave__
+
+#endif
