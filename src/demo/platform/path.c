aed235ecb1ba8279b7c5397d18d8c1e7060588cc
ruki <waruqi@gmail.com>
2022-03-01 23:25:44 +0800

    improve path

--- src/demo/platform/path.c
+++ src/demo/platform/path.c
@@ -4,14 +4,52 @@
 #include "../demo.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
+ * private implementation
+ */
+
+static tb_void_t tb_demo_path_test_directory(tb_char_t const* path, tb_char_t const* excepted)
+{
+    tb_char_t data[TB_PATH_MAXN] = {0};
+    tb_char_t const* result = tb_path_directory(path, data, sizeof(data));
+    if (result && excepted && !tb_strcmp(result, excepted))
+        tb_trace_i("directory(%s): %s passed!", path, result);
+    else if (!result && !excepted)
+        tb_trace_i("directory(%s): null passed!", path);
+    else tb_trace_i("directory(%s): %s != %s", path, result, excepted);
+}
+
+static tb_void_t tb_demo_path_test_absolute(tb_char_t const* path, tb_char_t const* excepted)
+{
+}
+
+static tb_void_t tb_demo_path_test_relative(tb_char_t const* path, tb_char_t const* excepted)
+{
+}
+
+static tb_void_t tb_demo_path_test_translate(tb_char_t const* path, tb_char_t const* excepted)
+{
+}
+
+/* //////////////////////////////////////////////////////////////////////////////////////
  * main
  */
 tb_int_t tb_demo_platform_path_main(tb_int_t argc, tb_char_t** argv)
 {
-    // the absolute path
-    tb_char_t path[TB_PATH_MAXN] = {0};
-//    tb_trace_i("%s", tb_path_directory(argv[1], path, TB_PATH_MAXN));
-//    tb_trace_i("%s", tb_path_relative_to(argv[2], argv[1], path, TB_PATH_MAXN));
-    tb_trace_i("%s", tb_path_absolute_to(argv[2], argv[1], path, TB_PATH_MAXN));
+    tb_demo_path_test_directory("", tb_null);
+    tb_demo_path_test_directory(".", tb_null);
+    tb_demo_path_test_directory("/tmp", "/");
+    tb_demo_path_test_directory("/tmp/", "/tmp");
+    tb_demo_path_test_directory("/tmp/xxx", "/tmp");
+    tb_demo_path_test_directory("/", "/");
+#ifdef TB_CONFIG_OS_WINDOWS
+    tb_demo_path_test_directory("c:", tb_null);
+    tb_demo_path_test_directory("c:\\", "c:");
+    tb_demo_path_test_directory("c:\\xxx", "c:");
+    tb_demo_path_test_directory("c:\\xxx\\yyy", "c:\\xxx");
+#endif
+
+    tb_demo_path_test_absolute("", "");
+    tb_demo_path_test_relative("", "");
+    tb_demo_path_test_translate("", "");
     return 0;
 }
