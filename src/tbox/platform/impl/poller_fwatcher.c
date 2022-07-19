7dc684ce4a2dc4f162a7399cc15150d408ad0ff7
ruki <waruqi@gmail.com>
2022-07-20 00:52:30 +0800

    hook fwatcher to poller

--- src/tbox/platform/impl/poller_fwatcher.c
+++ src/tbox/platform/impl/poller_fwatcher.c
@@ -32,5 +32,84 @@
 #include "../../container/container.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+// the poller fwatcher type
+typedef struct __tb_poller_fwatcher_t
+{
+    // the main poller
+    tb_poller_t*            main_poller;
+
+    // the fwatcher poller thread
+    tb_thread_ref_t         thread;
+
+}tb_poller_fwatcher_t;
+
+
+/* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
  */
+static tb_void_t tb_poller_fwatcher_kill(tb_poller_fwatcher_ref_t self)
+{
+}
+static tb_void_t tb_poller_fwatcher_exit(tb_poller_fwatcher_ref_t self)
+{
+}
+static tb_poller_fwatcher_ref_t tb_poller_fwatcher_init(tb_poller_t* main_poller)
+{
+    tb_bool_t             ok = tb_false;
+    tb_poller_fwatcher_t* poller = tb_null;
+    do
+    {
+        // @note only support one fwatcher poller instance
+        static tb_size_t s_poller_fwatcher_num = 0;
+        if (s_poller_fwatcher_num++)
+        {
+            tb_trace_e("only support one fwatcher poller!");
+            break;
+        }
+
+        // make the fwatcher poller
+        poller = tb_malloc0_type(tb_poller_fwatcher_t);
+        tb_assert_and_check_break(poller);
+
+        // save the main poller
+        poller->main_poller = main_poller;
+
+        // ok
+        ok = tb_true;
+
+    } while (0);
+
+    // failed? exit the poller
+    if (!ok)
+    {
+        if (poller) tb_poller_fwatcher_exit((tb_poller_fwatcher_ref_t)poller);
+        poller = tb_null;
+    }
+    return (tb_poller_fwatcher_ref_t)poller;
+}
+static tb_void_t tb_poller_fwatcher_spak(tb_poller_fwatcher_ref_t self)
+{
+}
+static tb_bool_t tb_poller_fwatcher_insert(tb_poller_fwatcher_ref_t self, tb_fwatcher_ref_t fwatcher, tb_cpointer_t priv)
+{
+    return tb_true;
+}
+static tb_bool_t tb_poller_fwatcher_modify(tb_poller_fwatcher_ref_t self, tb_fwatcher_ref_t fwatcher, tb_cpointer_t priv)
+{
+    return tb_true;
+}
+static tb_bool_t tb_poller_fwatcher_remove(tb_poller_fwatcher_ref_t self, tb_fwatcher_ref_t fwatcher)
+{
+    return tb_true;
+}
+static tb_bool_t tb_poller_fwatcher_wait_prepare(tb_poller_fwatcher_ref_t self)
+{
+    return 0;
+}
+static tb_long_t tb_poller_fwatcher_wait_poll(tb_poller_fwatcher_ref_t self, tb_poller_event_func_t func)
+{
+    return -1;
+}
