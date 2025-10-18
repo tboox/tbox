33d38b9a71c835c5e0c5385cb950491b4780ca28
ruki <waruqi@gmail.com>
2025-10-18 22:02:26 +0800

    improve buffer resize #287

--- src/tbox/memory/buffer.c
+++ src/tbox/memory/buffer.c
@@ -42,63 +42,43 @@
  */
 tb_bool_t tb_buffer_init(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return_val(buffer, tb_false);
 
-    // init
     buffer->data = buffer->buff;
     buffer->size = 0;
     buffer->maxn = sizeof(buffer->buff);
-
-    // ok
     return tb_true;
 }
 tb_void_t tb_buffer_exit(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return(buffer);
 
-    // clear it
     tb_buffer_clear(buffer);
 
-    // exit data
     if (buffer->data && buffer->data != buffer->buff) tb_free(buffer->data);
     buffer->data = buffer->buff;
 
-    // exit size
     buffer->size = 0;
     buffer->maxn = sizeof(buffer->buff);
 }
 tb_byte_t* tb_buffer_data(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return_val(buffer, tb_null);
-
-    // the buffer data
     return buffer->data;
 }
 tb_size_t tb_buffer_size(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return_val(buffer, 0);
-
-    // the buffer size
     return buffer->size;
 }
 tb_size_t tb_buffer_maxn(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return_val(buffer, 0);
-
-    // the buffer maxn
     return buffer->maxn;
 }
 tb_void_t tb_buffer_clear(tb_buffer_ref_t buffer)
 {
-    // check
     tb_assert_and_check_return(buffer);
-
-    // clear it
     buffer->size = 0;
 }
 tb_byte_t* tb_buffer_resize(tb_buffer_ref_t buffer, tb_size_t size)
@@ -106,7 +86,6 @@ tb_byte_t* tb_buffer_resize(tb_buffer_ref_t buffer, tb_size_t size)
     // check
     tb_assert_and_check_return_val(buffer && size, tb_null);
 
-    // done
     tb_bool_t   ok = tb_false;
     tb_byte_t*  buff_data = buffer->data;
     tb_size_t   buff_size = buffer->size;
@@ -119,34 +98,26 @@ tb_byte_t* tb_buffer_resize(tb_buffer_ref_t buffer, tb_size_t size)
         // using static buffer?
         if (buff_data == buffer->buff)
         {
-            // grow?
             if (size > buff_maxn)
             {
-                // grow maxn
-                buff_maxn = tb_align8(size + TB_BUFFER_GROW_SIZE);
+                buff_maxn = tb_align_pow2(size + TB_BUFFER_GROW_SIZE);
                 tb_assert_and_check_break(size <= buff_maxn);
 
-                // grow data
                 buff_data = tb_malloc_bytes(buff_maxn);
                 tb_assert_and_check_break(buff_data);
 
-                // copy data
                 tb_memcpy(buff_data, buffer->buff, buff_size);
             }
 
-            // update the size
             buff_size = size;
         }
         else
         {
-            // grow?
             if (size > buff_maxn)
             {
-                // grow maxn
-                buff_maxn = tb_align8(size + TB_BUFFER_GROW_SIZE);
+                buff_maxn = tb_align_pow2(size + TB_BUFFER_GROW_SIZE);
                 tb_assert_and_check_break(size <= buff_maxn);
 
-                // grow data
                 buff_data = (tb_byte_t*)tb_ralloc(buff_data, buff_maxn);
                 tb_assert_and_check_break(buff_data);
             }
@@ -154,38 +125,24 @@ tb_byte_t* tb_buffer_resize(tb_buffer_ref_t buffer, tb_size_t size)
             // decrease to the static buffer
             else if (size <= sizeof(buffer->buff))
             {
-                // update the maxn
                 buff_maxn = sizeof(buffer->buff);
-
-                // copy data
                 tb_memcpy(buffer->buff, buff_data, size);
-
-                // free data
                 tb_free(buff_data);
-
-                // using the static buffer
                 buff_data = buffer->buff;
             }
 #endif
-
-            // update the size
             buff_size = size;
         }
 
-        // update the buffer
         buffer->data = buff_data;
         buffer->size = buff_size;
         buffer->maxn = buff_maxn;
-
-        // ok
         ok = tb_true;
 
     } while (0);
 
-    // trace
     tb_assertf(ok, "resize buffer failed: %lu => %lu", buff_size, size);
 
-    // ok
     return ok? (tb_byte_t*)buffer->data : tb_null;
 }
 tb_byte_t* tb_buffer_memset(tb_buffer_ref_t buffer, tb_byte_t b)
@@ -206,8 +163,6 @@ tb_byte_t* tb_buffer_memnsetp(tb_buffer_ref_t buffer, tb_size_t p, tb_byte_t b, 
 {
     // check
     tb_assert_and_check_return_val(buffer, tb_null);
-
-    // check
     tb_check_return_val(n, tb_buffer_data(buffer));
 
     // resize
@@ -216,8 +171,6 @@ tb_byte_t* tb_buffer_memnsetp(tb_buffer_ref_t buffer, tb_size_t p, tb_byte_t b, 
 
     // memset
     tb_memset(d + p, b, n);
-
-    // ok?
     return d;
 }
 tb_byte_t* tb_buffer_memcpy(tb_buffer_ref_t buffer, tb_buffer_ref_t b)
@@ -236,8 +189,6 @@ tb_byte_t* tb_buffer_memncpyp(tb_buffer_ref_t buffer, tb_size_t p, tb_byte_t con
 {
     // check
     tb_assert_and_check_return_val(buffer && b, tb_null);
-
-    // check
     tb_check_return_val(n, tb_buffer_data(buffer));
 
     // resize
@@ -246,8 +197,6 @@ tb_byte_t* tb_buffer_memncpyp(tb_buffer_ref_t buffer, tb_size_t p, tb_byte_t con
 
     // copy it
     tb_memcpy(d + p, b, n);
-
-    // ok
     return d;
 }
 tb_byte_t* tb_buffer_memmov(tb_buffer_ref_t buffer, tb_size_t b)
@@ -285,7 +234,6 @@ tb_byte_t* tb_buffer_memnmovp(tb_buffer_ref_t buffer, tb_size_t p, tb_size_t b, 
     tb_byte_t* d = tb_buffer_resize(buffer, p + n);
     tb_assert_and_check_return_val(d, tb_null);
 
-    // memmov
     tb_memmov(d + p, d + b, n);
     return d;
 }
@@ -297,8 +245,6 @@ tb_byte_t* tb_buffer_memncat(tb_buffer_ref_t buffer, tb_byte_t const* b, tb_size
 {
     // check
     tb_assert_and_check_return_val(buffer && b, tb_null);
-
-    // check
     tb_check_return_val(n, tb_buffer_data(buffer));
 
     // is null?
@@ -311,8 +257,6 @@ tb_byte_t* tb_buffer_memncat(tb_buffer_ref_t buffer, tb_byte_t const* b, tb_size
 
     // memcat
     tb_memcpy(d + p, b, n);
-
-    // ok?
     return d;
 }
 
